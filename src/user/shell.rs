4c85ad8b18b8d3bd6a4f1bec9ef8575ddaa85451
Vincent Ollivier <v@vinc.cc>
2019-12-31 23:14:17 +0100

    Avoid adding empty commands to history

--- src/user/shell.rs
+++ src/user/shell.rs
@@ -27,21 +27,20 @@ impl Shell {
                 }
                 '\n' => {
                     print!("\n");
-
-                    // Remove first command from history if full
-                    if self.history.len() == self.history.capacity() {
-                        let first = self.history.iter().next().unwrap().clone();
-                        self.history.remove(&first);
-                    }
-
-                    // Add or move command to history at the end
-                    let cmd = self.cmd.clone();
-                    self.history.remove(&cmd);
-                    if self.history.insert(cmd).is_ok() {
-                        self.history_index = self.history.len();
-                    }
-
                     if self.cmd.len() > 0 {
+                        // Remove first command from history if full
+                        if self.history.len() == self.history.capacity() {
+                            let first = self.history.iter().next().unwrap().clone();
+                            self.history.remove(&first);
+                        }
+
+                        // Add or move command to history at the end
+                        let cmd = self.cmd.clone();
+                        self.history.remove(&cmd);
+                        if self.history.insert(cmd).is_ok() {
+                            self.history_index = self.history.len();
+                        }
+
                         let line = self.cmd.clone();
                         let args: Vec<&str, U256> = line.split_whitespace().collect();
                         match args[0] {
