03e9744e6723953130284ad888d6ee1a4764472b
Vincent Ollivier <v@vinc.cc>
2020-01-06 22:45:54 +0100

    Update cool-retro-term script

--- run/cool-retro-term.sh
+++ run/cool-retro-term.sh
@@ -4,9 +4,8 @@ image="target/x86_64-moros/release/bootimage-moros.bin"
 qemu="qemu-system-x86_64 -display curses -cpu max"
 
 # Build image if needed
-if [ ! -f "$dir/../$image" ]; then
-  cd "$dir/.." && cargo xbuild --release
-fi
+cd "$dir/.." && cargo bootimage --release
 
+# Launch qemu inside cool-retro-term
 cool-retro-term --fullscreen --profile "$dir/cool-retro-term.json" \
   --workdir "$dir/.." -e sh -c "$qemu $image"
