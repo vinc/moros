9cb9ef392df7458accfae079da584197310c1e46
Vincent Ollivier <v@vinc.cc>
2020-01-12 09:32:36 +0100

    Start over with a full screen editor

--- src/user/editor.rs
+++ src/user/editor.rs
@@ -8,17 +8,9 @@ pub fn main(args: &[&str]) -> user::shell::ExitCode {
     }
 
     let pathname = args[1];
-    if pathname.starts_with("/dev") || pathname.starts_with("/sys") {
-        print!("Permission denied to write to '{}'\n", pathname);
-        user::shell::ExitCode::CommandError
-    } else if let Some(mut file) = kernel::fs::File::create(pathname) {
-        let contents = input();
-        file.write(&contents);
-        user::shell::ExitCode::CommandSuccessful
-    } else {
-        print!("Could not write to '{}'\n", pathname);
-        user::shell::ExitCode::CommandError
-    }
+    let mut editor = Editor::new(pathname);
+    editor.run();
+    user::shell::ExitCode::CommandSuccessful
 }
 
 fn input() -> String<U2048> {
@@ -31,4 +23,70 @@ fn input() -> String<U2048> {
         output.push_str(&line).ok(); // TODO: File full
     }
     output
+}
+
+pub struct Editor {
+    pathname: String<U256>,
+}
+
+impl Editor {
+    pub fn new(pathname: &str) -> Self {
+        Self {
+            pathname: pathname.into(),
+        }
+    }
+
+    pub fn run(&mut self) {
+        kernel::vga::clear_screen();
+        loop {
+            let (x, y) = kernel::vga::cursor_position();
+            let c = kernel::console::get_char();
+            match c {
+                '\0' => {
+                    continue;
+                }
+                '\x03' => { // Ctrl C
+                    return;
+                },
+                '\n' => { // Newline
+                    print!("\n");
+                },
+                '↑' => { // Arrow up
+                    if y > 0 {
+                        kernel::vga::set_cursor_position(x, y - 1);
+                        kernel::vga::set_writer_position(x, y - 1);
+                    }
+                },
+                '↓' => { // Arrow down
+                    if y < kernel::vga::screen_height() - 1 {
+                        kernel::vga::set_cursor_position(x, y + 1);
+                        kernel::vga::set_writer_position(x, y + 1);
+                    }
+                },
+                '←' => { // Arrow left
+                    if x > 0 {
+                        kernel::vga::set_cursor_position(x - 1, y);
+                        kernel::vga::set_writer_position(x - 1, y);
+                    }
+                },
+                '→' => { // Arrow right
+                    if x < kernel::vga::screen_width() - 1 {
+                        kernel::vga::set_cursor_position(x + 1, y);
+                        kernel::vga::set_writer_position(x + 1, y);
+                    }
+                },
+                '\x08' => { // Backspace
+                    if x > 0 {
+                        kernel::vga::set_cursor_position(x - 1, y);
+                        kernel::vga::set_writer_position(x - 1, y);
+                    }
+                },
+                c => {
+                    if c.is_ascii_graphic() || c.is_ascii_whitespace() {
+                        print!("{}", c);
+                    }
+                },
+            }
+        }
+    }
 }
