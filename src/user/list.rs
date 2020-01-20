4b96b5033c6790332af5161c6d998c0072683fa8
Vincent Ollivier <v@vinc.cc>
2020-01-20 08:58:20 +0100

    Use sort from alloc Vec

--- src/user/list.rs
+++ src/user/list.rs
@@ -15,36 +15,9 @@ pub fn main(args: &[&str]) -> user::shell::ExitCode {
     }
 
     if let Some(dir) = kernel::fs::Dir::open(pathname) {
-        //let mut files: Vec<_> = dir.read().collect();
         let mut files: Vec<_> = dir.read().collect();
 
-        // With `std::Vec` we would have used the following to sort the files:
-        //
-        // files.sort_by_key(|f| f.name());
-        let n = files.len();
-        for i in 0..n {
-            let mut min = i;
-            for j in (i + 1)..n {
-                if files[j].name().as_str() < files[min].name().as_str() {
-                    min = j
-                }
-            }
-            if min != i {
-                // The implementation of `heapless::String` doesn't have Copy,
-                // so `DirEntry` that use it doesn't either, therefor the
-                // following is not possible:
-                //
-                // let tmp = files[i];
-                // files[i] = files[min];
-                // files[min] = tmp;
-                let tmp = files.swap_remove(min);
-                //files.push(tmp);
-                files.push(tmp);
-                let tmp = files.swap_remove(i);
-                //files.push(tmp);
-                files.push(tmp);
-            }
-        }
+        files.sort_by_key(|f| f.name());
 
         for file in files {
             print!("{}\n", file.name());
