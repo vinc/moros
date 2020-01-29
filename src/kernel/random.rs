bba149012ccf483a6747d1f4ce593dc06c44f6df
Vincent Ollivier <v@vinc.cc>
2020-01-29 09:36:54 +0100

    Add kernel::random::rand16

--- src/kernel/random.rs
+++ src/kernel/random.rs
@@ -6,3 +6,10 @@ pub fn rand64() -> Option<u64> {
         None => None
     }
 }
+
+pub fn rand16() -> Option<u16> {
+    match RdRand::new() {
+        Some(rand) => rand.get_u16(),
+        None => None
+    }
+}
