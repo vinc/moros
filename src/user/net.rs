aeb89107553bcca8a517fb89e951964837557285
Vincent Ollivier <v@vinc.cc>
2020-01-30 23:00:24 +0100

    Add subcommand config and dump to net command

--- src/user/net.rs
+++ src/user/net.rs
@@ -3,61 +3,73 @@ use crate::{print, kernel, user};
 use smoltcp::time::Instant;
 use smoltcp::socket::{SocketSet, TcpSocket, TcpSocketBuffer};
 
-pub fn main(_args: &[&str]) -> user::shell::ExitCode {
+pub fn main(args: &[&str]) -> user::shell::ExitCode {
+    if args.len() == 1 {
+        print!("Usage: net <command>\n");
+        return user::shell::ExitCode::CommandError;
+    }
+
     if let Some(ref mut iface) = *kernel::rtl8139::IFACE.lock() {
-        iface.device_mut().debug_mode = true;
-
-        let mut server_rx_buffer = [0; 2048];
-        let mut server_tx_buffer = [0; 2048];
-        let server_socket = TcpSocket::new(
-            TcpSocketBuffer::new(&mut server_rx_buffer[..]),
-            TcpSocketBuffer::new(&mut server_tx_buffer[..])
-        );
-
-        /*
-        match server_socket.connect((Ipv4Address::new(10, 0, 2, 2), 8000), 4242) {
-            Ok(_) => {
-                print!("Socket connected\n");
-            },
-            Err(e) => {
-                print!("Socket error: {}\n", e);
-            },
-        }
-        if server_socket.is_open() {
-            print!("Socket is open\n");
-            if server_socket.can_send() {
-                print!("Socket can send\n");
-                let buf = "Hello, World!".as_bytes();
-                server_socket.send_slice(&buf);
-            } else {
-                print!("Socket cannot send\n");
-            }
-        } else {
-            print!("Socket is not open\n");
-        }
-        */
-
-        let mut sockets_storage = [None, None];
-        let mut sockets = SocketSet::new(&mut sockets_storage[..]);
-        let _server_handle = sockets.add(server_socket);
-
-        loop {
-            let now = kernel::clock::clock_monotonic();
-            match iface.poll(&mut sockets, Instant::from_millis((now * 1000.0) as i64)) {
-                Ok(true) => {
-                    print!("------------------------------------------------------------------\n");
-                    print!("Polling result: Ok(true)\n");
-                },
-                Ok(false) => {
-                    //print!("------------------------------------------------------------------\n");
-                    //print!("Polling Result: Ok(false)\n\n");
-                },
-                Err(e) => {
-                    print!("------------------------------------------------------------------\n");
-                    print!("polling result: err({})\n", e);
+        match args[1] {
+            "config" => {
+                if args.len() < 4 {
+                    print!("Usage: net config <key> <value>\n");
+                    return user::shell::ExitCode::CommandError;
+                }
+                match args[2] {
+                    "debug" => {
+                        iface.device_mut().debug_mode = match args[3] {
+                            "1" | "on" | "enable" => true,
+                            "0" | "off" | "disable" => false,
+                            _ => {
+                                print!("Invalid config value\n");
+                                return user::shell::ExitCode::CommandError;
+                            }
+                        }
+                    }
+                    _ => {
+                        print!("Invalid config key\n");
+                        return user::shell::ExitCode::CommandError;
+                    }
                 }
             }
-            kernel::time::sleep(1.0);
+            "dump" => {
+                iface.device_mut().debug_mode = true;
+
+                let mut server_rx_buffer = [0; 2048];
+                let mut server_tx_buffer = [0; 2048];
+                let server_socket = TcpSocket::new(
+                    TcpSocketBuffer::new(&mut server_rx_buffer[..]),
+                    TcpSocketBuffer::new(&mut server_tx_buffer[..])
+                );
+
+                let mut sockets_storage = [None, None];
+                let mut sockets = SocketSet::new(&mut sockets_storage[..]);
+                let _server_handle = sockets.add(server_socket);
+
+                loop {
+                    let now = kernel::clock::clock_monotonic();
+                    match iface.poll(&mut sockets, Instant::from_millis((now * 1000.0) as i64)) {
+                        Ok(true) => {
+                            print!("------------------------------------------------------------------\n");
+                            print!("Polling result: Ok(true)\n");
+                        },
+                        Ok(false) => {
+                            //print!("------------------------------------------------------------------\n");
+                            //print!("Polling Result: Ok(false)\n\n");
+                        },
+                        Err(e) => {
+                            print!("------------------------------------------------------------------\n");
+                            print!("polling result: err({})\n", e);
+                        }
+                    }
+                    kernel::time::sleep(1.0);
+                }
+            }
+            _ => {
+                print!("Invalid command\n");
+                return user::shell::ExitCode::CommandError;
+            }
         }
     }
     user::shell::ExitCode::CommandSuccessful
