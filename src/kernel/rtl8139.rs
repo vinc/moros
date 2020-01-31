756e5e6656e16e7527eacac0e8c2ec59a5864d6d
Vincent Ollivier <v@vinc.cc>
2020-01-31 23:31:54 +0100

    Add more comments to code

--- src/kernel/rtl8139.rs
+++ src/kernel/rtl8139.rs
@@ -17,12 +17,35 @@ use spin::Mutex;
 use x86_64::instructions::port::Port;
 use x86_64::VirtAddr;
 
+// 00 = 8K + 16 bytes
+// 01 = 16K + 16 bytes
+// 10 = 32K + 16 bytes
+// 11 = 64K + 16 bytes
+const RX_BUFFER_IDX: usize = 0;
+
 const MTU: usize = 1500;
-const RX_BUFFER_LEN: usize = 8192 + 16;
+
+const RX_BUFFER_PAD: usize = 16;
+const RX_BUFFER_LEN: usize = (8129 << RX_BUFFER_IDX) + RX_BUFFER_PAD;
+
 const RX_BUFFER_EMPTY: u8 = 0x01;
-const TX_BUFFER_LEN: usize = 4096 + 16;
+const TX_BUFFER_LEN: usize = 4096;
 const TX_BUFFERS_COUNT: usize = 4;
-const ISR_ROK: u16 = 0x01;
+const ROK: u16 = 0x01;
+
+// Rx Buffer Length
+const RCR_RBLEN: u32 = (RX_BUFFER_IDX << 11) as u32;
+
+// When the WRAP bit is set, the nic will keep moving the rest
+// of the packet data into the memory immediately after the
+// end of the Rx buffer instead of going back to the begining
+// of the buffer. So the buffer must have an additionnal 1500 bytes.
+const RCR_WRAP: u32 = 1 << 7;
+
+const RCR_AB: u32 = 1 << 3; // Accept Broadcast packets
+const RCR_AM: u32 = 1 << 2; // Accept Multicast packets
+const RCR_APM: u32 = 1 << 1; // Accept Physical Match packets
+const RCR_AAP: u32 = 1; // Accept All Packets
 
 // Interframe Gap Time
 const TCR_IFG: u32 = 3 << 24;
@@ -83,7 +106,7 @@ impl Ports {
             ],
             config1: Port::new(io_addr + 0x52),
             rx_addr: Port::new(io_addr + 0x30),
-            rx_ptr: Port::new(io_addr + 0x38),
+            rx_ptr: Port::new(io_addr + 0x38), // CAPR
             cmd: Port::new(io_addr + 0x37),
             imr: Port::new(io_addr + 0x3C),
             isr: Port::new(io_addr + 0x3E),
@@ -124,7 +147,7 @@ impl RTL8139 {
             ports: Ports::new(io_addr),
             eth_addr: None,
 
-            // Add MTU to RX_BUFFER_LEN if WRAP is set
+            // Add MTU to RX_BUFFER_LEN if RCR_WRAP is set
             rx_buffer: Box::new(vec![0; RX_BUFFER_LEN + MTU]),
 
             rx_offset: 0,
@@ -192,14 +215,8 @@ impl RTL8139 {
         // Set IMR + ISR
         unsafe { self.ports.imr.write(0x0005) }
 
-        // When the WRAP bit is set, the nic will keep moving the rest
-        // of the packet data into the memory immediately after the end
-        // of the Rx buffer instead of going back to the begining of the
-        // buffer. So the buffer must have an additionnal 1500 bytes.
-        let wrap = 1 << 7;
-
         // Configuring Receive buffer (RCR)
-        unsafe { self.ports.rx_config.write((0xF | wrap) as u32) }
+        unsafe { self.ports.rx_config.write(RCR_RBLEN | RCR_WRAP | RCR_AB | RCR_AM | RCR_APM | RCR_AAP) }
 
         // Configuring Transmit buffer (TCR)
         unsafe { self.ports.tx_config.write(TCR_IFG | TCR_MXDMA0 | TCR_MXDMA1 | TCR_MXDMA2); }
@@ -240,6 +257,7 @@ impl<'a> Device<'a> for RTL8139 {
         if (cmd & RX_BUFFER_EMPTY) == RX_BUFFER_EMPTY {
             return None;
         }
+        let isr = unsafe { self.ports.isr.read() };
         let offset = self.rx_offset;
         let header = u16::from_le_bytes(self.rx_buffer[(offset + 0)..(offset + 2)].try_into().unwrap());
         if self.debug_mode {
@@ -247,26 +265,27 @@ impl<'a> Device<'a> for RTL8139 {
             let uptime = kernel::clock::clock_monotonic();
             print!("[{:.6}] NET RTL8139 receiving buffer:\n\n", uptime);
             print!("Command Register: 0x{:02X}\n", cmd);
+            print!("Interrupt Status Register: 0x{:02X}\n", isr);
             print!("Header: 0x{:04X}\n", header);
         }
-        if header & ISR_ROK != ISR_ROK {
+        if header & ROK != ROK {
             return None;
         }
         let length = u16::from_le_bytes(self.rx_buffer[(offset + 2)..(offset + 4)].try_into().unwrap());
         let n = length as usize;
         let crc = u32::from_le_bytes(self.rx_buffer[(offset + n)..(offset + n + 4)].try_into().unwrap());
 
-        // Update buffer read pointer
-        self.rx_offset = (offset + n + 4 + 3) & !3;
-        unsafe { self.ports.rx_ptr.write((self.rx_offset - 16) as u16); }
-        self.rx_offset %= RX_BUFFER_LEN;
-
         if self.debug_mode {
             print!("Length: {} bytes\n", n - 4);
             print!("CRC: 0x{:08X}\n", crc);
-            user::hex::print_hex(&self.rx_buffer[(offset + 4)..(offset + n)]);
             print!("RX Offset: {}\n", self.rx_offset);
+            user::hex::print_hex(&self.rx_buffer[(offset + 4)..(offset + n)]);
         }
+
+        // Update buffer read pointer
+        self.rx_offset = (offset + n + 4 + 3) & !3;
+        unsafe { self.ports.rx_ptr.write((self.rx_offset - RX_BUFFER_PAD) as u16); }
+        self.rx_offset %= RX_BUFFER_LEN;
 
         let state = &self.state;
         let rx = RxToken { state, buffer: &mut self.rx_buffer[(offset + 4)..(offset + n)] };
@@ -322,13 +341,13 @@ pub struct TxToken<'a> {
     debug_mode: bool,
 }
 
-const CRS: u32 = 1 << 31; // Carrier Sense Lost
-const TAB: u32 = 1 << 30; // Transmit Abort
-const OWC: u32 = 1 << 29; // Out of Window Collision
-const CDH: u32 = 1 << 28; // CD Heart Beat
+//const CRS: u32 = 1 << 31; // Carrier Sense Lost
+//const TAB: u32 = 1 << 30; // Transmit Abort
+//const OWC: u32 = 1 << 29; // Out of Window Collision
+//const CDH: u32 = 1 << 28; // CD Heart Beat
 const TOK: u32 = 1 << 15; // Transmit OK
-const TUN: u32 = 1 << 15; // Transmit FIFO Underrun
-const OWN: u32 = 1 << 15; // DMA operation completed
+//const TUN: u32 = 1 << 14; // Transmit FIFO Underrun
+const OWN: u32 = 1 << 13; // DMA operation completed
 
 impl<'a> phy::TxToken for TxToken<'a> {
     fn consume<R, F>(mut self, _timestamp: Instant, len: usize, f: F) -> Result<R>
@@ -345,10 +364,11 @@ impl<'a> phy::TxToken for TxToken<'a> {
                 // 3. Fill in Transmit Status: the size of this packet, the
                 // early transmit threshold, and clear OWN bit in TSD (this
                 // starts the PCI operation).
-                // NOTE: the size of the packet fit the first 12 bits, and
-                // a value of 000000 for the early transmit threshold means
-                // 8 bytes. So we just write the size of the packet.
-                self.cmd_port.write(0xFFF & len as u32);
+                // NOTE: The length of the packet use the first 13 bits (but
+                // should not exceed 1792 bytes), and a value of 0x000000
+                // for the early transmit threshold means 8 bytes. So we
+                // just write the size of the packet.
+                self.cmd_port.write(0x1FFF & len as u32);
 
                 // 4. When the whole packet is moved to FIFO, the OWN bit is
                 // set to 1.
