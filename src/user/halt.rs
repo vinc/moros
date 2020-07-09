668a19808f618e4c3b2601f053a333366d9656a8
Vincent Ollivier <v@vinc.cc>
2020-07-09 20:47:21 +0200

    Add color to halt command

--- src/user/halt.rs
+++ src/user/halt.rs
@@ -1,7 +1,9 @@
 use crate::{print, kernel, user};
 
 pub fn main(_args: &[&str]) -> user::shell::ExitCode {
-    print!("MOROS has reached its fate, the system is now halted.\n");
+    let csi_color = kernel::console::color("Yellow");
+    let csi_reset = kernel::console::color("Reset");
+    print!("{}MOROS has reached its fate, the system is now halted.{}\n", csi_color, csi_reset);
     kernel::time::sleep(3.0);
     kernel::acpi::poweroff();
     user::shell::ExitCode::CommandSuccessful
