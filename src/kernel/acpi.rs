fad9f43d9ce04349ab439fe576f9d2d43fb89cdf
Vincent Ollivier <v@vinc.cc>
2020-11-08 10:55:58 +0100

    Refactor code

--- src/kernel/acpi.rs
+++ src/kernel/acpi.rs
@@ -2,15 +2,40 @@ use crate::{kernel, log};
 use acpi::{AcpiHandler, PhysicalMapping, AcpiTables};
 use alloc::boxed::Box;
 use aml::{AmlContext, AmlName, DebugVerbosity, Handler};
+use aml::value::AmlValue;
 use x86_64::instructions::port::Port;
 use x86_64::PhysAddr;
+
+#[allow(dead_code)]
+#[repr(u64)]
+enum FADT {
+    SciInterrupt     = 46, // u16,
+    SmiCmdPort       = 48, // u32,
+    AcpiEnable       = 52, // u8,
+    AcpiDisable      = 53, // u8,
+    S4biosReq        = 54, // u8,
+    PstateControl    = 55, // u8,
+    Pm1aEventBlock   = 56, // u32,
+    Pm1bEventBlock   = 60, // u32,
+    Pm1aControlBlock = 64, // u32,
+    Pm1bControlBlock = 68, // u32,
+}
+
+fn read_addr<T>(physical_address: usize) -> T where T: Copy {
+    let virtual_address = kernel::mem::phys_to_virt(PhysAddr::new(physical_address as u64));
+    unsafe { *virtual_address.as_ptr::<T>() }
+}
+
+fn read_fadt<T>(address: usize, offset: FADT) -> T where T: Copy {
+    read_addr::<T>(address + offset as usize)
+}
 
 pub fn shutdown() {
     let mut pm1a_control_block = 0;
     let mut slp_typa = 0;
     let slp_len = 1 << 13;
 
-    log!("ACPI Executing shutdown\n");
+    log!("ACPI Shutdown\n");
     let mut aml = AmlContext::new(Box::new(MorosAmlHandler), false, DebugVerbosity::None);
     let res = unsafe { AcpiTables::search_for_rsdp_bios(MorosAcpiHandler) };
     match res {
@@ -19,10 +44,18 @@ pub fn shutdown() {
             for (sign, sdt) in acpi.sdts {
                 if sign.as_str() == "FACP" {
                     //log!("ACPI Found FACP at {}\n", sdt.physical_address);
-                    let offset = 64; // PM1a is 64 bytes into FACP
-                    let virtual_address = kernel::mem::phys_to_virt(PhysAddr::new(offset + sdt.physical_address as u64));
-                    pm1a_control_block = unsafe { *virtual_address.as_ptr::<u32>() };
-                    //log!("ACPI Found PM1a Control Block: {}\n", pm1a_control_block);
+
+                    /*
+                    // Enable ACPI
+                    let smi_cmd_port = read_fadt::<u16>(sdt.physical_address, FADT::SmiCmdPort);
+                    let acpi_enable = read_fadt::<u8>(sdt.physical_address, FADT::AcpiEnable);
+                    let mut port: Port<u8> = Port::new(smi_cmd_port);
+                    unsafe { port.write(acpi_enable); }
+                    kernel::time::sleep(3.0);
+                    */
+
+                    pm1a_control_block = read_fadt::<u32>(sdt.physical_address, FADT::Pm1aControlBlock);
+                    //log!("ACPI Found PM1a Control Block: 0x{:x}\n", pm1a_control_block);
                 }
             }
             match &acpi.dsdt {
@@ -30,17 +63,17 @@ pub fn shutdown() {
                     //log!("ACPI Found DSDT at {}\n", dsdt.address);
                     let address = kernel::mem::phys_to_virt(PhysAddr::new(dsdt.address as u64));
                     let stream = unsafe { core::slice::from_raw_parts(address.as_ptr(), dsdt.length as usize) };
-                    if aml.parse_table(stream).is_err() {
-                        log!("ACPI Failed to parse AML in DSDT\n");
-                        return;
-                    }
-                    let name = AmlName::from_str("\\_S5").unwrap();
-                    if let Ok(aml::value::AmlValue::Package(s5)) = aml.namespace.get_by_path(&name) {
-                        //log!("ACPI Found \\_S5 in DSDT\n");
-                        if let aml::value::AmlValue::Integer(value) = s5[0] {
-                            slp_typa = value;
-                            //log!("ACPI Found SLP_TYPa in \\_S5: {}\n", slp_typa);
+                    if aml.parse_table(stream).is_ok() {
+                        let name = AmlName::from_str("\\_S5").unwrap();
+                        if let Ok(AmlValue::Package(s5)) = aml.namespace.get_by_path(&name) {
+                            //log!("ACPI Found \\_S5 in DSDT\n");
+                            if let AmlValue::Integer(value) = s5[0] {
+                                slp_typa = value as u16;
+                                //log!("ACPI Found SLP_TYPa in \\_S5: {}\n", slp_typa);
+                            }
                         }
+                    } else {
+                        log!("ACPI Failed to parse AML in DSDT\n");
                     }
                 },
                 None => {},
@@ -53,7 +86,7 @@ pub fn shutdown() {
 
     let mut port: Port<u16> = Port::new(pm1a_control_block as u16);
     unsafe {
-        port.write(slp_typa as u16 | slp_len as u16);
+        port.write(slp_typa | slp_len);
     }
 }
 
@@ -76,22 +109,13 @@ impl AcpiHandler for MorosAcpiHandler {
     }
 }
 
-macro_rules! def_read_handler {
-    ($name:ident, $size:ty) => {
-        fn $name(&self, physical_address: usize) -> $size {
-            let virtual_address = kernel::mem::phys_to_virt(PhysAddr::new(physical_address as u64));
-            unsafe { *virtual_address.as_ptr::<$size>() }
-        }
-    };
-}
-
 struct MorosAmlHandler;
 
 impl Handler for MorosAmlHandler {
-    def_read_handler!(read_u8, u8);
-    def_read_handler!(read_u16, u16);
-    def_read_handler!(read_u32, u32);
-    def_read_handler!(read_u64, u64);
+    fn read_u8(&self, address: usize) -> u8 { read_addr::<u8>(address) }
+    fn read_u16(&self, address: usize) -> u16 { read_addr::<u16>(address) }
+    fn read_u32(&self, address: usize) -> u32 { read_addr::<u32>(address) }
+    fn read_u64(&self, address: usize) -> u64 { read_addr::<u64>(address) }
     fn write_u8(&mut self, _address: usize, _value: u8) { unimplemented!() }
     fn write_u16(&mut self, _address: usize, _value: u16) { unimplemented!() }
     fn write_u32(&mut self, _address: usize, _value: u32) { unimplemented!() }
