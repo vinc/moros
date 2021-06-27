25d5428e661a75a2c6e89186cc6815af5d3b01cd
Vincent Ollivier <v@vinc.cc>
2021-06-27 16:56:24 +0200

    Add virtualbox script

---
+++ run/virtualbox.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+# TODO: Add steps to create image with VBoxManage createvm
+make image output=vga keyboard=dvorak nic=pcnet
+qemu-img convert -f raw -O vdi disk.img disk.vdi -o size=32M
+VBoxManage internalcommands sethduuid disk.vdi dbbfad68-c3d1-4c9a-828f-7e4db4e9488e
+VBoxManage startvm Moros
