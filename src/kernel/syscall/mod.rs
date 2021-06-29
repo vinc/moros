b3297927c3a6b019991b10637b277026906277e9
Vincent Ollivier <v@vinc.cc>
2021-06-29 22:03:46 +0200

    Add syscall macro

--- src/kernel/syscall/mod.rs
+++ src/kernel/syscall/mod.rs
@@ -1,3 +1,4 @@
+#[doc(hidden)]
 pub unsafe fn syscall0(n: usize) -> usize {
     let ret: usize;
     asm!(
@@ -7,6 +8,7 @@ pub unsafe fn syscall0(n: usize) -> usize {
     ret
 }
 
+#[doc(hidden)]
 pub unsafe fn syscall1(n: usize, arg1: usize) -> usize {
     let ret: usize;
     asm!(
@@ -17,6 +19,7 @@ pub unsafe fn syscall1(n: usize, arg1: usize) -> usize {
     ret
 }
 
+#[doc(hidden)]
 pub unsafe fn syscall2(n: usize, arg1: usize, arg2: usize) -> usize {
     let ret: usize;
     asm!(
@@ -27,6 +30,7 @@ pub unsafe fn syscall2(n: usize, arg1: usize, arg2: usize) -> usize {
     ret
 }
 
+#[doc(hidden)]
 pub unsafe fn syscall3(n: usize, arg1: usize, arg2: usize, arg3: usize) -> usize {
     let ret: usize;
     asm!(
@@ -37,6 +41,7 @@ pub unsafe fn syscall3(n: usize, arg1: usize, arg2: usize, arg3: usize) -> usize
     ret
 }
 
+#[doc(hidden)]
 pub unsafe fn syscall4(n: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize) -> usize {
     let ret: usize;
     asm!(
@@ -45,4 +50,23 @@ pub unsafe fn syscall4(n: usize, arg1: usize, arg2: usize, arg3: usize, arg4: us
         lateout("rax") ret
     );
     ret
+}
+
+#[macro_export]
+macro_rules! syscall {
+    ($n:expr) => (
+        $crate::kernel::syscall::syscall0(
+            $n as usize));
+    ($n:expr, $a1:expr) => (
+        $crate::kernel::syscall::syscall1(
+            $n as usize, $a1 as usize));
+    ($n:expr, $a1:expr, $a2:expr) => (
+        $crate::kernel::syscall::syscall2(
+            $n as usize, $a1 as usize, $a2 as usize));
+    ($n:expr, $a1:expr, $a2:expr, $a3:expr) => (
+        $crate::kernel::syscall::syscall3(
+            $n as usize, $a1 as usize, $a2 as usize, $a3 as usize));
+    ($n:expr, $a1:expr, $a2:expr, $a3:expr, $a4:expr) => (
+        $crate::kernel::syscall::syscall4(
+            $n as usize, $a1 as usize, $a2 as usize, $a3 as usize, $a4 as usize));
 }
