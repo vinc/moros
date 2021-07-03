fc3476eb0f2b92476dfe6c52fc490fc7f5d35c5c
Vincent Ollivier <v@vinc.cc>
2021-07-03 09:53:45 +0200

    Remove syscall4

--- src/kernel/syscall/mod.rs
+++ src/kernel/syscall/mod.rs
@@ -1,13 +1,11 @@
 pub mod number;
 pub mod service;
 
-use lazy_static::lazy_static;
-
 /*
  * Dispatching system calls
  */
 
-pub fn dispatcher(n: usize, arg1: usize, arg2: usize, arg3: usize) -> usize {
+pub fn dispatcher(n: usize, arg1: usize, _arg2: usize, _arg3: usize) -> usize {
     match n {
         number::SLEEP => {
             // sleep(f64)
@@ -30,56 +28,45 @@ pub fn dispatcher(n: usize, arg1: usize, arg2: usize, arg3: usize) -> usize {
 
 #[doc(hidden)]
 pub unsafe fn syscall0(n: usize) -> usize {
-    let ret: usize;
+    let res: usize;
     asm!(
         "int 0x80", in("rax") n,
-        lateout("rax") ret
+        lateout("rax") res
     );
-    ret
+    res
 }
 
 #[doc(hidden)]
 pub unsafe fn syscall1(n: usize, arg1: usize) -> usize {
-    let ret: usize;
+    let res: usize;
     asm!(
         "int 0x80", in("rax") n,
         in("rdi") arg1,
-        lateout("rax") ret
+        lateout("rax") res
     );
-    ret
+    res
 }
 
 #[doc(hidden)]
 pub unsafe fn syscall2(n: usize, arg1: usize, arg2: usize) -> usize {
-    let ret: usize;
+    let res: usize;
     asm!(
         "int 0x80", in("rax") n,
         in("rdi") arg1, in("rsi") arg2,
-        lateout("rax") ret
+        lateout("rax") res
     );
-    ret
+    res
 }
 
 #[doc(hidden)]
 pub unsafe fn syscall3(n: usize, arg1: usize, arg2: usize, arg3: usize) -> usize {
-    let ret: usize;
+    let res: usize;
     asm!(
         "int 0x80", in("rax") n,
         in("rdi") arg1, in("rsi") arg2, in("rdx") arg3,
-        lateout("rax") ret
+        lateout("rax") res
     );
-    ret
-}
-
-#[doc(hidden)]
-pub unsafe fn syscall4(n: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize) -> usize {
-    let ret: usize;
-    asm!(
-        "int 0x80", in("rax") n,
-        in("rdi") arg1, in("rsi") arg2, in("rdx") arg3, in("rcx") arg4,
-        lateout("rax") ret
-    );
-    ret
+    res
 }
 
 #[macro_export]
@@ -96,7 +83,4 @@ macro_rules! syscall {
     ($n:expr, $a1:expr, $a2:expr, $a3:expr) => (
         $crate::kernel::syscall::syscall3(
             $n as usize, $a1 as usize, $a2 as usize, $a3 as usize));
-    ($n:expr, $a1:expr, $a2:expr, $a3:expr, $a4:expr) => (
-        $crate::kernel::syscall::syscall4(
-            $n as usize, $a1 as usize, $a2 as usize, $a3 as usize, $a4 as usize));
 }
