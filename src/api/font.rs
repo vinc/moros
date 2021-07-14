1cace98fe1895bf96cc16d3c7af3fecaaea99792
Vincent Ollivier <v@vinc.cc>
2021-07-14 23:26:38 +0200

    Add tests to api::font::from_bytes

--- src/api/font.rs
+++ src/api/font.rs
@@ -9,6 +9,7 @@ pub struct Font {
 
 // http://www.fifi.org/doc/console-tools-dev/file-formats/psf
 pub fn from_bytes(buf: &[u8]) -> Result<Font, ()> {
+    // Header
     if buf.len() < 4 || buf[0] != 0x36 || buf[1] != 0x04 {
         return Err(());
     }
@@ -19,10 +20,25 @@ pub fn from_bytes(buf: &[u8]) -> Result<Font, ()> {
         1 | 3 => 512,
         _ => return Err(()),
     };
+
+    // Data
     let n = (4 + size * height as u16) as usize;
     if buf.len() < n {
         return Err(());
     }
     let data = buf[4..n].to_vec();
+
+    // TODO: Unicode Table
+
     Ok(Font { height, size, data })
+}
+
+#[test_case]
+fn parse_psf_font() {
+    assert!(from_bytes(include_bytes!("../../dsk/ini/boot.sh")).is_err());
+
+    let font = from_bytes(include_bytes!("../../dsk/ini/fonts/zap-light-8x16.psf")).unwrap();
+    assert_eq!(font.height, 16);
+    assert_eq!(font.size, 256);
+    assert_eq!(font.data.len(), 256 * 16);
 }
