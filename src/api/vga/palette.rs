2841ad18ac48b1b642967db287ce42ed2e5552f7
Vincent Ollivier <v@vinc.cc>
2021-07-14 23:08:32 +0200

    Refactor palette::from_str with tests

--- src/api/vga/palette.rs
+++ src/api/vga/palette.rs
@@ -6,21 +6,33 @@ pub struct Palette {
 }
 
 pub fn from_csv(s: &str) -> Result<Palette, ()> {
-    let mut colors = Vec::with_capacity(16);
-    for row in s.split("\n") {
-        let row = row.split("#").next().unwrap();
-        let color: Vec<u8> = row.split(",").filter_map(|value| {
+    let colors: Vec<_> = s.split("\n").filter_map(|line| {
+        let line = line.split("#").next().unwrap(); // Remove comments
+        let color: Vec<u8> = line.split(",").filter_map(|value| {
             let radix = if value.contains("0x") { 16 } else { 10 };
             let value = value.trim().trim_start_matches("0x");
             u8::from_str_radix(value, radix).ok()
         }).collect();
-        if color.len() == 4 {
-            colors.push((color[0], color[1], color[2], color[3]));
+        if color.len() == 4 { // Color index + rgb values
+            Some((color[0], color[1], color[2], color[3]))
+        } else {
+            None
         }
-    }
-    if let Ok(colors) = colors.try_into() {
+    }).collect();
+    if let Ok(colors) = colors.try_into() { // Array of 16 colors
         Ok(Palette { colors })
     } else {
         Err(())
     }
+}
+
+#[test_case]
+fn parse_palette_csv() {
+    assert!(from_csv("").is_err());
+    assert!(from_csv("0,0,0,0").is_err());
+
+    let s = include_str!("../../../dsk/ini/palette.csv");
+    let palette = from_csv(s).unwrap();
+    assert_eq!(palette.colors[0x03].0, 0x03);
+    assert_eq!(palette.colors[0x0D].2, 0x86);
 }
