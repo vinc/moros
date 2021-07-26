c6e922bc227aac31f0cad22a8fcca6de6a53dc9b
Vincent Ollivier <vincent.ollivier@augment.com>
2021-07-26 21:35:44 +0200

    Add sort parameter to list command

--- src/usr/list.rs
+++ src/usr/list.rs
@@ -5,30 +5,45 @@ use alloc::vec::Vec;
 use time::OffsetDateTime;
 
 pub fn main(args: &[&str]) -> usr::shell::ExitCode {
-    let current_dir = sys::process::dir();
-    let mut pathname = if args.len() == 2 && !args[1].is_empty() {
-        args[1]
-    } else {
-        &current_dir
-    };
+    let mut path: &str = &sys::process::dir();
+    let mut sort = "name";
+
+    let mut i = 1;
+    let n = args.len();
+    while i < n {
+        if args[i] == "--sort" && i + 1 < n {
+            sort = args[i + 1];
+            i += 2;
+        } else {
+            path = args[i];
+            i += 1;
+        }
+    }
 
     // The commands `list /usr/alice/` and `list /usr/alice` are equivalent,
     // but `list /` should not be modified.
-    if pathname.len() > 1 {
-        pathname = pathname.trim_end_matches('/');
+    if path.len() > 1 {
+        path = path.trim_end_matches('/');
     }
 
-    if let Some(dir) = sys::fs::Dir::open(pathname) {
+    if let Some(dir) = sys::fs::Dir::open(path) {
         let mut files: Vec<_> = dir.read().collect();
 
-        files.sort_by_key(|f| f.name());
-
-        //let w = files.map(|f| f.size()).to_string().len();
-        let mut w = 0;
-        for file in &files {
-            w = core::cmp::max(w, file.size());
+        match sort {
+            "name" => files.sort_by_key(|f| f.name()),
+            "size" => files.sort_by_key(|f| f.size()),
+            "time" => files.sort_by_key(|f| f.time()),
+            _ => {
+                println!("Invalid sort key '{}'", sort);
+                return usr::shell::ExitCode::CommandError;
+            }
         }
-        let w = w.to_string().len();
+
+        let mut max_size = 0;
+        for file in &files {
+            max_size = core::cmp::max(max_size, file.size());
+        }
+        let width = max_size.to_string().len();
 
         let csi_color = Style::color("Blue");
         let csi_reset = Style::reset();
@@ -36,11 +51,11 @@ pub fn main(args: &[&str]) -> usr::shell::ExitCode {
         for file in files {
             let date = OffsetDateTime::from_unix_timestamp(file.time() as i64);
             let color = if file.is_dir() { csi_color } else { csi_reset };
-            println!("{:w$} {} {}{}{}", file.size(), date.format("%F %H:%M:%S"), color, file.name(), csi_reset, w = w);
+            println!("{:width$} {} {}{}{}", file.size(), date.format("%F %H:%M:%S"), color, file.name(), csi_reset, width = width);
         }
         usr::shell::ExitCode::CommandSuccessful
     } else {
-        println!("Dir not found '{}'", pathname);
+        println!("Dir not found '{}'", path);
         usr::shell::ExitCode::CommandError
     }
 }
