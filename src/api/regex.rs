0f8c3db530a9717371c2b4e88ef237804734a5b7
Vincent Ollivier <vincent.ollivier@augment.com>
2021-07-29 10:32:29 +0200

    Fix off by one error

--- src/api/regex.rs
+++ src/api/regex.rs
@@ -6,6 +6,7 @@ pub struct Regex(String);
 
 impl Regex {
     pub fn new(re: &str) -> Self {
+        //println!("debug: new('{}')", re);
         Self(re.to_string())
     }
     pub fn is_match(&self, text: &str) -> bool {
@@ -14,7 +15,7 @@ impl Regex {
 }
 
 fn is_match(re: &str, text: &str) -> bool {
-    println!("debug: is_match('{}', '{}')", re, text);
+    //println!("debug: is_match('{}', '{}')", re, text);
     if re.chars().nth(0) == Some('^') {
         return is_match_here(&re[1..], text);
     }
@@ -30,7 +31,7 @@ fn is_match(re: &str, text: &str) -> bool {
 }
 
 fn is_match_here(re: &str, text: &str) -> bool {
-    println!("debug: is_match_here('{}', '{}')", re, text);
+    //println!("debug: is_match_here('{}', '{}')", re, text);
     if re.len() == 0 {
         return true;
     }
@@ -38,7 +39,7 @@ fn is_match_here(re: &str, text: &str) -> bool {
         return is_match_star(re.chars().nth(0).unwrap(), &re[2..], text);
     }
     if re.chars().nth(0) == Some('$') && re.len() == 1 {
-        return text.len() == 1;
+        return text.len() == 0;
     }
     if text.len() != 0 && (re.chars().nth(0) == Some('.') || re.chars().nth(0) == text.chars().nth(0)) {
         return is_match_here(&re[1..], &text[1..]);
@@ -47,10 +48,10 @@ fn is_match_here(re: &str, text: &str) -> bool {
 }
 
 fn is_match_star(c: char, re: &str, text: &str) -> bool {
-    println!("debug: is_match_star('{}', '{}', '{}')", c, re, text);
+    //println!("debug: is_match_star('{}', '{}', '{}')", c, re, text);
     let mut i = 0;
     let n = text.len();
-    while i < n && (text.chars().nth(i) == Some(c) || c == '.') {
+    while i <= n && (text.chars().nth(i) == Some(c) || c == '.') {
         if is_match_here(re, &text[i..]) {
             return true;
         }
@@ -67,6 +68,11 @@ fn test_regex() {
     assert!(Regex::new("a.a").is_match("aba"));
     assert!(!Regex::new("a.a").is_match("abb"));
     assert!(Regex::new("a.*").is_match("abb"));
-    assert!(Regex::new("^.*$").is_match("aaa"));
+    assert!(Regex::new(".*").is_match("aaa"));
     assert!(Regex::new("^a.*a$").is_match("aaa"));
+    assert!(Regex::new("^#.*").is_match("#aaa"));
+    assert!(!Regex::new("^#.*").is_match("a#aaa"));
+    assert!(Regex::new(".*;$").is_match("aaa;"));
+    assert!(!Regex::new(".*;$").is_match("aaa;a"));
+    assert!(Regex::new("^.*$").is_match("aaa"));
 }
