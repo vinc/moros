e1eb27ed8981bfc916124d665ce788843b01fda2
Vincent Ollivier <v@vinc.cc>
2021-08-06 01:12:13 +0200

    Refactor code

--- src/sys/process.rs
+++ src/sys/process.rs
@@ -59,22 +59,23 @@ pub fn set_user(user: &str) {
 }
 
 
+/************************
+ * Userspace experiment *
+ ************************/
 
+// See https://nfil.dev/kernel/rust/coding/rust-kernel-to-userspace-and-back/
+// And https://github.com/WartaPoirier-corp/ananos/blob/dev/docs/notes/context-switch.md
+
+use crate::sys::gdt::GDT;
+use core::sync::atomic::AtomicU64;
 use x86_64::VirtAddr;
 use x86_64::instructions::interrupts;
-use x86_64::structures::paging::{
-    Page, PageTableFlags,
-};
-use x86_64::structures::{
-    paging::{Mapper, FrameAllocator, Size4KiB},
-};
-use core::sync::atomic::AtomicU64;
-use crate::sys::gdt::GDT;
+use x86_64::structures::paging::{Mapper, FrameAllocator, Size4KiB};
+use x86_64::structures::paging::{Page, PageTableFlags};
 
-// TODO: use virtual memory better, i.e don't map all
-// processes in the same page table directory
 static STACK_ADDR: AtomicU64 = AtomicU64::new(0x600_000);
 static CODE_ADDR: AtomicU64 = AtomicU64::new(0x400_000);
+const PAGE_SIZE: u64 = 1024 * 4;
 
 pub struct Process {
     stack_addr: u64,
@@ -83,61 +84,42 @@ pub struct Process {
 
 impl Process {
     pub fn create(mapper: &mut impl Mapper<Size4KiB>, frame_alloc: &mut impl FrameAllocator<Size4KiB>, asm: &[u8]) -> Process {
-        const PAGE_SIZE: u64 = 1024 * 4; 
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::USER_ACCESSIBLE;
 
-        let stack = STACK_ADDR.fetch_add(PAGE_SIZE, Ordering::SeqCst);
+        let stack_addr = STACK_ADDR.fetch_add(PAGE_SIZE, Ordering::SeqCst);
         let frame = frame_alloc.allocate_frame().unwrap();
-        let page = Page::containing_address(VirtAddr::new(stack));
+        let page = Page::containing_address(VirtAddr::new(stack_addr));
         unsafe {
             mapper.map_to(page, frame, flags, frame_alloc).unwrap().flush();
         }
 
-        let code = CODE_ADDR.fetch_add(PAGE_SIZE, Ordering::SeqCst);
+        let code_addr = CODE_ADDR.fetch_add(PAGE_SIZE, Ordering::SeqCst);
         let frame = frame_alloc.allocate_frame().unwrap();
-        let page = Page::containing_address(VirtAddr::new(code));
+        let page = Page::containing_address(VirtAddr::new(code_addr));
         unsafe {
             mapper.map_to(page, frame, flags, frame_alloc).unwrap().flush();
         }
 
         unsafe {
-            let code = code as *mut u8;
+            let code_addr = code_addr as *mut u8;
             for (i, op) in asm.iter().enumerate() {
-                core::ptr::write(code.add(i), *op);
+                core::ptr::write(code_addr.add(i), *op);
             }
         }
 
-        Process {
-            stack_addr: stack,
-            code_addr: code,
-        }
+        Process { stack_addr, code_addr }
     }
 
     pub fn switch(&self) {
-        crate::println!("DEBUG: switching to userspace");
+        println!("DEBUG: switching to userspace");
         let data = GDT.1.user_data.0;
         let code = GDT.1.user_code.0;
-
         unsafe {
             interrupts::disable();
-
             asm!(
-                //"mov ds, ax",
-                //"mov es, ax",
-                //"mov fs, ax",
-                //"mov gs, ax",
-
                 "push rax",
                 "push rsi",
                 "push 0x200",
-                
-                // Reenable interrupts in userspace
-                //"pushf", // Get EFLAGS
-                //"pop rax",
-                //"or rax, 0x200", // Set IF
-                //"push rax",
-                
-                //"push rcx",
                 "push rdx",
                 "push rdi",
                 "iretq",
