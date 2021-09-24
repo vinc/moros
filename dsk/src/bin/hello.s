5773a876264517be2d832c5641efce7de57317ae
Vincent Ollivier <vincent.ollivier@augment.com>
2021-09-24 09:59:33 +0200

    Rename entry label

--- dsk/src/bin/hello.s
+++ dsk/src/bin/hello.s
@@ -1,14 +1,14 @@
 [bits 64]
-global _start
 
+section .data
+msg: db "Hello, World!", 10
+
+global _start
 section .text
-start:
+_start:
   mov rax, 5                ; syscall number for WRITE
   mov rdi, 1                ; standard output
   mov rsi, msg              ; addr of string
   mov rdx, 14               ; size of string
   int 0x80
-  jmp start
-
-section .data
-msg: db "Hello, World!", 10
+  jmp _start
--- dsk/src/bin/sleep.s
+++ dsk/src/bin/sleep.s
@@ -1,8 +1,8 @@
 [bits 64]
-start:
+_start:
   mov rax, 0                ; syscall number for SLEEP
   mov rdi, __float64__(1.0) ; time to sleep in seconds
   mov rsi, 0
   mov rdx, 0
   int 0x80
-  jmp start
+  jmp _start
