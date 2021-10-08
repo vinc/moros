87a65390d359bb08e0fb6f512f2f771f5be1a0ab
Vincent Ollivier <vincent.ollivier@augment.com>
2021-10-08 13:54:25 +0200

    Simplify binaries

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,28 +1,17 @@
 #![no_std]
 #![no_main]
-#![feature(asm)]
-#![feature(naked_functions)]
 
 use moros::api::syscall;
 use core::panic::PanicInfo;
 
-#[no_mangle]
-#[naked]
-pub unsafe extern "sysv64" fn _start() -> ! {
-    asm!(
-        "call {}",
-        sym main,
-        options(noreturn)
-    )
-}
-
-fn main(_argc: isize, _argv: *mut *const u8) {
-    loop {
-        syscall::write(1, b"Hello, World!\n");
-    }
-}
-
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+#[no_mangle]
+pub unsafe extern "sysv64" fn _start() -> ! {
+    loop {
+        syscall::write(1, b"Hello, World!\n");
+    }
 }
