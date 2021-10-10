5ddffe15b927ada752d95ea3df7e7febc73bef94
Vincent Ollivier <v@vinc.cc>
2021-10-10 21:32:03 +0200

    Fix stack overflow

--- src/sys/process.rs
+++ src/sys/process.rs
@@ -126,12 +126,13 @@ use x86_64::VirtAddr;
 use x86_64::structures::paging::{Mapper, FrameAllocator};
 use x86_64::structures::paging::{Page, PageTableFlags};
 
-static CODE_ADDR: AtomicU64 = AtomicU64::new(0x40_0000);
-static STACK_ADDR: AtomicU64 = AtomicU64::new(0x80_0000);
+static STACK_ADDR: AtomicU64 = AtomicU64::new(0x200_0000);
+static CODE_ADDR: AtomicU64 = AtomicU64::new(0x100_0000);
 const PAGE_SIZE: u64 = 4 * 1024;
 
 pub struct Process {
     stack_addr: u64,
+    stack_size: u64,
     code_addr: u64,
     entry_point: u64,
 }
@@ -141,20 +142,32 @@ impl Process {
         let mut mapper = unsafe { sys::mem::mapper(VirtAddr::new(sys::mem::PHYS_MEM_OFFSET)) };
         let mut frame_allocator = unsafe { sys::mem::BootInfoFrameAllocator::init(sys::mem::MEMORY_MAP.unwrap()) };
 
+
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::USER_ACCESSIBLE;
 
-        let stack_addr = STACK_ADDR.fetch_add(PAGE_SIZE, Ordering::SeqCst);
-        let frame = frame_allocator.allocate_frame().unwrap();
-        let page = Page::containing_address(VirtAddr::new(stack_addr));
-        unsafe {
-            mapper.map_to(page, frame, flags, &mut frame_allocator).unwrap().flush();
+        let stack_size = 256 * PAGE_SIZE;
+        let stack_addr = STACK_ADDR.fetch_add(stack_size, Ordering::SeqCst);
+        let pages = {
+            let stack_start_page = Page::containing_address(VirtAddr::new(stack_addr));
+            let stack_end_page = Page::containing_address(VirtAddr::new(stack_addr + stack_size));
+            Page::range_inclusive(stack_start_page, stack_end_page)
+        };
+        for page in pages {
+            let frame = frame_allocator.allocate_frame().unwrap();
+            unsafe {
+                mapper.map_to(page, frame, flags, &mut frame_allocator).unwrap().flush();
+            }
         }
 
-        let code_addr = CODE_ADDR.fetch_add(PAGE_SIZE, Ordering::SeqCst);
-        let frame = frame_allocator.allocate_frame().unwrap();
-        for i in 0..1024 {
-            let addr = code_addr + i * PAGE_SIZE;
-            let page = Page::containing_address(VirtAddr::new(addr));
+        let code_size = 1024 * PAGE_SIZE;
+        let code_addr = CODE_ADDR.fetch_add(code_size, Ordering::SeqCst);
+        let pages = {
+            let code_start_page = Page::containing_address(VirtAddr::new(code_addr));
+            let code_end_page = Page::containing_address(VirtAddr::new(code_addr + code_size));
+            Page::range_inclusive(code_start_page, code_end_page)
+        };
+        for page in pages {
+            let frame = frame_allocator.allocate_frame().unwrap();
             unsafe {
                 mapper.map_to(page, frame, flags, &mut frame_allocator).unwrap().flush();
             }
@@ -188,7 +201,7 @@ impl Process {
 
         set_code_addr(code_addr);
 
-        Ok(Process { stack_addr, code_addr, entry_point })
+        Ok(Process { stack_addr, stack_size, code_addr, entry_point })
     }
 
     // Switch to user mode and execute the program
@@ -206,7 +219,7 @@ impl Process {
                 "push rdi",   // Instruction pointer (RIP)
                 "iretq",
                 in("rax") data,
-                in("rsi") self.stack_addr,
+                in("rsi") self.stack_addr + self.stack_size,
                 in("rdx") code,
                 in("rdi") self.code_addr + self.entry_point,
             );
