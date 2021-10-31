5003532e937381129d5e23a51d1b116d97ea210c
Vincent Ollivier <vincent.ollivier@augment.com>
2021-10-31 10:03:53 +0100

    Reorder syscalls

--- dsk/src/bin/hello.s
+++ dsk/src/bin/hello.s
@@ -6,7 +6,7 @@ msg: db "Hello, World!", 10
 global _start
 section .text
 _start:
-  mov rax, 5                ; syscall number for WRITE
+  mov rax, 4                ; syscall number for WRITE
   mov rdi, 1                ; standard output
   mov rsi, msg              ; addr of string
   mov rdx, 14               ; size of string
