74ac80334137cded861e07ff5c215f7d873d5885
Vincent Ollivier <v@vinc.cc>
2021-11-14 17:16:09 +0100

    Revert max file handle value to fix crash

--- src/sys/process.rs
+++ src/sys/process.rs
@@ -7,7 +7,7 @@ use lazy_static::lazy_static;
 use object::{Object, ObjectSegment};
 use spin::RwLock;
 
-const MAX_FILE_HANDLES: usize = 32;
+const MAX_FILE_HANDLES: usize = 16;
 const MAX_PROCS: usize = 2; // TODO: Update this when EXIT syscall is working
 
 lazy_static! {
