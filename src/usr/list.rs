73130711524b52fa6abed9b3b8aa05ed26a8e7d6
Vincent Ollivier <vincent.ollivier@augment.com>
2022-01-16 15:35:57 +0100

    Allow listing of singular file

--- src/usr/list.rs
+++ src/usr/list.rs
@@ -2,6 +2,9 @@ use crate::{sys, usr};
 use crate::api::console::Style;
 use crate::api::time;
 use crate::api::fs;
+use crate::api::syscall;
+use crate::sys::fs::FileStat;
+
 use alloc::string::ToString;
 use alloc::vec::Vec;
 
@@ -28,47 +31,62 @@ pub fn main(args: &[&str]) -> usr::shell::ExitCode {
         path = path.trim_end_matches('/');
     }
 
-    if let Ok(entries) = fs::read_dir(path) {
-        let mut files: Vec<_> = entries.iter().filter(|entry| {
-            !(entry.name().starts_with('.') && hide_dot_files)
-        }).collect();
+    if let Some(stat) = syscall::stat(path) {
+        if stat.is_dir() {
+            if let Ok(entries) = fs::read_dir(path) {
+                let mut files: Vec<_> = entries.iter().filter(|entry| {
+                    !(entry.name().starts_with('.') && hide_dot_files)
+                }).collect();
 
-        match sort {
-            "name" => files.sort_by_key(|f| f.name()),
-            "size" => files.sort_by_key(|f| f.size()),
-            "time" => files.sort_by_key(|f| f.time()),
-            _ => {
-                eprintln!("Invalid sort key '{}'", sort);
-                return usr::shell::ExitCode::CommandError;
-            }
-        }
+                match sort {
+                    "name" => files.sort_by_key(|f| f.name()),
+                    "size" => files.sort_by_key(|f| f.size()),
+                    "time" => files.sort_by_key(|f| f.time()),
+                    _ => {
+                        eprintln!("Invalid sort key '{}'", sort);
+                        return usr::shell::ExitCode::CommandError;
+                    }
+                }
 
-        let mut max_size = 0;
-        for file in &files {
-            max_size = core::cmp::max(max_size, file.size());
-        }
-        let width = max_size.to_string().len();
+                let mut max_size = 0;
+                for file in &files {
+                    max_size = core::cmp::max(max_size, file.size());
+                }
+                let width = max_size.to_string().len();
 
-        let csi_dir_color = Style::color("Blue");
-        let csi_dev_color = Style::color("Yellow");
-        let csi_reset = Style::reset();
 
-        for file in files {
-            let date = time::from_timestamp(file.time() as i64);
-            let color = if file.is_dir() {
-                csi_dir_color
-            } else if file.is_device() {
-                csi_dev_color
+                for file in files {
+                    print_file(file, width);
+                }
+                usr::shell::ExitCode::CommandSuccessful
             } else {
-                csi_reset
-            };
-            println!("{:width$} {} {}{}{}", file.size(), date.format("%F %H:%M:%S"), color, file.name(), csi_reset, width = width);
+                eprintln!("Could not read directory '{}'", path);
+                usr::shell::ExitCode::CommandError
+            }
+        } else {
+            print_file(&stat, stat.size().to_string().len());
+            usr::shell::ExitCode::CommandSuccessful
         }
-        usr::shell::ExitCode::CommandSuccessful
     } else {
-        eprintln!("Dir not found '{}'", path);
+        eprintln!("Could not find file or directory '{}'", path);
         usr::shell::ExitCode::CommandError
     }
+}
+
+fn print_file(file: &FileStat, width: usize) {
+    let csi_dir_color = Style::color("Blue");
+    let csi_dev_color = Style::color("Yellow");
+    let csi_reset = Style::reset();
+
+    let date = time::from_timestamp(file.time() as i64);
+    let color = if file.is_dir() {
+        csi_dir_color
+    } else if file.is_device() {
+        csi_dev_color
+    } else {
+        csi_reset
+    };
+    println!("{:width$} {} {}{}{}", file.size(), date.format("%F %H:%M:%S"), color, file.name(), csi_reset, width = width);
 }
 
 fn help() -> usr::shell::ExitCode {
