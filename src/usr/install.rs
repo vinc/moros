d734f4eb85fdd4a2f780b58431f299aa154b83e3
Vincent Ollivier <vincent.ollivier@augment.com>
2022-01-25 21:38:30 +0100

    Add missing comment

--- src/usr/install.rs
+++ src/usr/install.rs
@@ -23,7 +23,7 @@ pub fn copy_files(verbose: bool) {
     create_dir("/dev/clk", verbose); // Clocks
     create_dev("/dev/clk/uptime", DeviceType::File, verbose); // TODO
     create_dev("/dev/clk/realtime", DeviceType::File, verbose); // TODO
-    create_dev("/dev/rtc", DeviceType::File, verbose);
+    create_dev("/dev/rtc", DeviceType::File, verbose); // TODO
     create_dev("/dev/null", DeviceType::Null, verbose);
     create_dev("/dev/random", DeviceType::Random, verbose);
     create_dev("/dev/console", DeviceType::Console, verbose);
