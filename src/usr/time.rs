37b15b8ed62c7bc468a21f5d2a38edd67a45d442
Vincent Ollivier <v@vinc.cc>
2022-06-05 13:16:44 +0200

    Fix call to realtime

--- src/usr/time.rs
+++ src/usr/time.rs
@@ -1,14 +1,14 @@
 use crate::usr;
-use crate::api::clock;
+use crate::api::syscall;
 use crate::api::console::Style;
 
 pub fn main(args: &[&str]) -> usr::shell::ExitCode {
     let csi_color = Style::color("LightBlue");
     let csi_reset = Style::reset();
     let cmd = args[1..].join(" ");
-    let start = clock::realtime();
+    let start = syscall::realtime();
     usr::shell::exec(&cmd);
-    let duration = clock::realtime() - start;
+    let duration = syscall::realtime() - start;
     println!("{}Executed '{}' in {:.6}s{}", csi_color, cmd, duration, csi_reset);
     usr::shell::ExitCode::CommandSuccessful
 }
