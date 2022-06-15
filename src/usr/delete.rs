a09fa30eb675d024856b099b1e3a33dcc1332bae
Vincent Ollivier <v@vinc.cc>
2022-06-15 07:40:12 +0200

    Add support for multiple files in delete command

--- src/usr/delete.rs
+++ src/usr/delete.rs
@@ -3,34 +3,35 @@ use crate::api::syscall;
 use crate::api::fs;
 
 pub fn main(args: &[&str]) -> usr::shell::ExitCode {
-    if args.len() != 2 {
+    if args.len() < 2 {
         return usr::shell::ExitCode::CommandError;
     }
 
-    let mut pathname = args[1];
+    for arg in &args[1..] {
+        let mut pathname = arg.clone();
 
-    // The commands `delete /usr/alice/` and `delete /usr/alice` are equivalent,
-    // but `delete /` should not be modified.
-    if pathname.len() > 1 {
-        pathname = pathname.trim_end_matches('/');
-    }
+        // The commands `delete /usr/alice/` and `delete /usr/alice` are equivalent,
+        // but `delete /` should not be modified.
+        if pathname.len() > 1 {
+            pathname = pathname.trim_end_matches('/');
+        }
 
-    if !fs::exists(pathname) {
-        error!("File not found '{}'", pathname);
-        return usr::shell::ExitCode::CommandError;
-    }
+        if !fs::exists(pathname) {
+            error!("File not found '{}'", pathname);
+            return usr::shell::ExitCode::CommandError;
+        }
 
-    if let Some(info) = syscall::info(pathname) {
-        if info.is_dir() && info.size() > 0 {
-            error!("Directory '{}' not empty", pathname);
+        if let Some(info) = syscall::info(pathname) {
+            if info.is_dir() && info.size() > 0 {
+                error!("Directory '{}' not empty", pathname);
+                return usr::shell::ExitCode::CommandError;
+            }
+        }
+
+        if fs::delete(pathname).is_err() {
+            error!("Could not delete file '{}'", pathname);
             return usr::shell::ExitCode::CommandError;
         }
     }
-
-    if fs::delete(pathname).is_ok() {
-        usr::shell::ExitCode::CommandSuccessful
-    } else {
-        error!("Could not delete file '{}'", pathname);
-        usr::shell::ExitCode::CommandError
-    }
+    usr::shell::ExitCode::CommandSuccessful
 }
