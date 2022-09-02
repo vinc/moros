3dcca83417df885ead75977e11d6980a4f436625
Vincent Ollivier <v@vinc.cc>
2022-09-03 01:27:48 +0200

    Use alloc conditionally in hello command

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,11 +1,19 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
+use alloc::format;
+
 use moros::api::syscall;
 use moros::entry_point;
 
 entry_point!(main);
 
-fn main(_args: &[&str]) {
-    syscall::write(1, b"Hello, World!\n");
+fn main(args: &[&str]) {
+    if args.len() > 1 {
+        syscall::write(1, format!("Hello, {}!\n", args[1]).as_bytes());
+    } else {
+        syscall::write(1, b"Hello, World!\n");
+    }
 }
