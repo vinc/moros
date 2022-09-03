dc7e624fcb9036754e3c7a67ac3b96b1339374d1
Vincent Ollivier <v@vinc.cc>
2022-09-03 10:02:25 +0200

    Add comment to the page fault issue

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -12,6 +12,8 @@ entry_point!(main);
 
 fn main(args: &[&str]) {
     if args.len() > 1 {
+        // FIXME: This will result in a page fault exception for an address
+        // that's already mapped to the kernel stack
         syscall::write(1, format!("Hello, {}!\n", args[1]).as_bytes());
     } else {
         syscall::write(1, b"Hello, World!\n");
