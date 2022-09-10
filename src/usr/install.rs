5eac2e53c13510e5159bfdd51afafa2bab5168b4
Vincent Ollivier <v@vinc.cc>
2022-09-10 08:32:02 +0200

    Add pi.lsp example

--- src/usr/install.rs
+++ src/usr/install.rs
@@ -55,6 +55,7 @@ pub fn copy_files(verbose: bool) {
     create_dir("/tmp/lisp", verbose);
     copy_file("/tmp/lisp/factorial.lsp", include_bytes!("../../dsk/tmp/lisp/factorial.lsp"), verbose);
     copy_file("/tmp/lisp/fibonacci.lsp", include_bytes!("../../dsk/tmp/lisp/fibonacci.lsp"), verbose);
+    copy_file("/tmp/lisp/pi.lsp", include_bytes!("../../dsk/tmp/lisp/pi.lsp"), verbose);
 
     create_dir("/tmp/life", verbose);
     copy_file("/tmp/life/centinal.cells", include_bytes!("../../dsk/tmp/life/centinal.cells"), verbose);
