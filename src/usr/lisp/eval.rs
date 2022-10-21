e712e92b56129aba3adb8c59712bd17880172e53
Vincent Ollivier <v@vinc.cc>
2022-10-21 22:33:46 +0200

    Add TCO to Lisp

--- src/usr/lisp/eval.rs
+++ src/usr/lisp/eval.rs
@@ -83,17 +83,6 @@ fn eval_cond_args(args: &[Exp], env: &mut Rc<RefCell<Env>>) -> Result<Exp, Err> 
     Ok(Exp::List(vec![]))
 }
 
-fn eval_if_args(args: &[Exp], env: &mut Rc<RefCell<Env>>) -> Result<Exp, Err> {
-    ensure_length_gt!(args, 1);
-    if eval(&args[0], env)? == Exp::Bool(true) {
-        eval(&args[1], env)
-    } else if args.len() > 2 {
-        eval(&args[2], env)
-    } else {
-        Ok(Exp::List(vec![]))
-    }
-}
-
 pub fn eval_label_args(args: &[Exp], env: &mut Rc<RefCell<Env>>) -> Result<Exp, Err> {
     ensure_length_eq!(args, 2);
     match &args[0] {
@@ -138,14 +127,6 @@ fn eval_while_args(args: &[Exp], env: &mut Rc<RefCell<Env>>) -> Result<Exp, Err>
         }
     }
     Ok(res)
-}
-
-fn eval_lambda_args(args: &[Exp]) -> Result<Exp, Err> {
-    ensure_length_eq!(args, 2);
-    Ok(Exp::Lambda(Lambda {
-        params: Rc::new(args[0].clone()),
-        body: Rc::new(args[1].clone()),
-    }))
 }
 
 fn eval_defun_args(args: &[Exp], env: &mut Rc<RefCell<Env>>) -> Result<Exp, Err> {
@@ -198,76 +179,84 @@ fn eval_load_args(args: &[Exp], env: &mut Rc<RefCell<Env>>) -> Result<Exp, Err> 
     Ok(Exp::Bool(true))
 }
 
-pub const BUILT_INS: [&str; 24] = [
-    "quote", "atom", "eq", "car", "cdr", "cons", "cond", "label", "lambda", "define", "def",
-    "function", "fun", "fn", "if", "while", "defun", "defn", "apply", "eval", "progn", "begin", "do",
-    "load"
-];
-
-fn eval_built_in_form(exp: &Exp, args: &[Exp], env: &mut Rc<RefCell<Env>>) -> Option<Result<Exp, Err>> {
-    match exp {
-        Exp::Sym(s) => {
-            match s.as_ref() {
-                // Seven Primitive Operators
-                "quote"                              => Some(eval_quote_args(args)),
-                "atom"                               => Some(eval_atom_args(args, env)),
-                "eq"                                 => Some(eval_eq_args(args, env)),
-                "car"                                => Some(eval_car_args(args, env)),
-                "cdr"                                => Some(eval_cdr_args(args, env)),
-                "cons"                               => Some(eval_cons_args(args, env)),
-                "cond"                               => Some(eval_cond_args(args, env)),
-
-                // Two Special Forms
-                "label" | "define" | "def"           => Some(eval_label_args(args, env)),
-                "lambda" | "function" | "fun" | "fn" => Some(eval_lambda_args(args)),
-
-                "if"                                 => Some(eval_if_args(args, env)),
-                "set"                                => Some(eval_set_args(args, env)),
-                "while"                              => Some(eval_while_args(args, env)),
-                "defun" | "defn"                     => Some(eval_defun_args(args, env)),
-                "apply"                              => Some(eval_apply_args(args, env)),
-                "eval"                               => Some(eval_eval_args(args, env)),
-                "progn" | "begin" | "do"             => Some(eval_progn_args(args, env)),
-                "load"                               => Some(eval_load_args(args, env)),
-                _                                    => None,
-            }
-        },
-        _ => None,
-    }
-}
-
 pub fn eval_args(args: &[Exp], env: &mut Rc<RefCell<Env>>) -> Result<Vec<Exp>, Err> {
     args.iter().map(|x| eval(x, env)).collect()
 }
 
+pub const BUILT_INS: [&str; 24] = [
+    "quote", "atom", "eq", "car", "cdr", "cons", "cond", "label", "lambda", "define", "def",
+    "function", "fun", "fn", "if", "while", "defun", "defn", "apply", "eval", "progn", "begin",
+    "do", "load"
+];
+
 pub fn eval(exp: &Exp, env: &mut Rc<RefCell<Env>>) -> Result<Exp, Err> {
-    match exp {
-        Exp::Sym(key) => env_get(key, env),
-        Exp::Bool(_) => Ok(exp.clone()),
-        Exp::Num(_) => Ok(exp.clone()),
-        Exp::Str(_) => Ok(exp.clone()),
-        Exp::List(list) => {
-            ensure_length_gt!(list, 0);
-            let first_form = &list[0];
-            let args = &list[1..];
-            match eval_built_in_form(first_form, args, env) {
-                Some(res) => res,
-                None => {
-                    let first_eval = eval(first_form, env)?;
-                    match first_eval {
-                        Exp::Primitive(f) => {
-                            f(&eval_args(args, env)?)
-                        },
-                        Exp::Lambda(f) => {
-                            let mut env = lambda_env(f.params, args, env)?;
-                            eval(&f.body, &mut env)
-                        },
-                        _ => Err(Err::Reason("First form must be a function".to_string())),
+    let mut env = env.clone();
+    let mut exp = exp.clone();
+    loop {
+        match exp.clone() {
+            Exp::Sym(key) => return env_get(&key, &mut env),
+            Exp::Bool(_) => return Ok(exp.clone()),
+            Exp::Num(_) => return Ok(exp.clone()),
+            Exp::Str(_) => return Ok(exp.clone()),
+            Exp::List(list) => {
+                ensure_length_gt!(list, 0);
+                let first_form = &list[0];
+                let args = &list[1..];
+                match first_form {
+                    Exp::Sym(s) if s == "quote"  => return eval_quote_args(args),
+                    Exp::Sym(s) if s == "atom"   => return eval_atom_args(args, &mut env),
+                    Exp::Sym(s) if s == "eq"     => return eval_eq_args(args, &mut env),
+                    Exp::Sym(s) if s == "car"    => return eval_car_args(args, &mut env),
+                    Exp::Sym(s) if s == "cdr"    => return eval_cdr_args(args, &mut env),
+                    Exp::Sym(s) if s == "cons"   => return eval_cons_args(args, &mut env),
+                    Exp::Sym(s) if s == "cond"   => return eval_cond_args(args, &mut env),
+                    Exp::Sym(s) if s == "set"    => return eval_set_args(args, &mut env),
+                    Exp::Sym(s) if s == "while"  => return eval_while_args(args, &mut env),
+                    Exp::Sym(s) if s == "defun"  => return eval_defun_args(args, &mut env),
+                    Exp::Sym(s) if s == "defn"   => return eval_defun_args(args, &mut env),
+                    Exp::Sym(s) if s == "apply"  => return eval_apply_args(args, &mut env),
+                    Exp::Sym(s) if s == "eval"   => return eval_eval_args(args, &mut env),
+                    Exp::Sym(s) if s == "progn"  => return eval_progn_args(args, &mut env),
+                    Exp::Sym(s) if s == "begin"  => return eval_progn_args(args, &mut env),
+                    Exp::Sym(s) if s == "do"     => return eval_progn_args(args, &mut env),
+                    Exp::Sym(s) if s == "load"   => return eval_load_args(args, &mut env),
+                    Exp::Sym(s) if s == "label"  => return eval_label_args(args, &mut env),
+                    Exp::Sym(s) if s == "define" => return eval_label_args(args, &mut env),
+                    Exp::Sym(s) if s == "def"    => return eval_label_args(args, &mut env),
+                    Exp::Sym(s) if s == "lambda" || s == "function" || s == "fun" || s == "fn" => {
+                        ensure_length_eq!(args, 2);
+                        return Ok(Exp::Lambda(Lambda {
+                            params: Rc::new(args[0].clone()),
+                            body: Rc::new(args[1].clone()),
+                        }))
+                    }
+                    Exp::Sym(s) if s == "if" => {
+                        ensure_length_gt!(args, 1);
+                        if eval(&args[0], &mut env)? == Exp::Bool(true) {
+                            exp = args[1].clone();
+                        } else if args.len() > 2 {
+                            exp = args[2].clone();
+                        } else {
+                            exp = Exp::List(vec![]);
+                        }
+                    }
+                    _ => {
+                        let first_eval = eval(first_form, &mut env)?;
+                        match first_eval {
+                            Exp::Primitive(f) => {
+                                return f(&eval_args(args, &mut env)?)
+                            },
+                            Exp::Lambda(f) => {
+                                env = lambda_env(f.params, args, &mut env)?;
+                                exp = (*f.body).clone();
+                            },
+                            _ => return Err(Err::Reason("First form must be a function".to_string())),
+                        }
                     }
                 }
-            }
-        },
-        Exp::Primitive(_) => Err(Err::Reason("Unexpected form".to_string())),
-        Exp::Lambda(_) => Err(Err::Reason("Unexpected form".to_string())),
+            },
+            Exp::Primitive(_) => return Err(Err::Reason("Unexpected form".to_string())),
+            Exp::Lambda(_) => return Err(Err::Reason("Unexpected form".to_string())),
+        }
     }
 }
