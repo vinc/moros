454845d887100cb953b0c19a0f0175abffd3e14d
Vincent Ollivier <v@vinc.cc>
2022-11-06 10:00:04 +0100

    Add -b option to memory usage

--- src/usr/memory.rs
+++ src/usr/memory.rs
@@ -1,36 +1,70 @@
 use crate::sys;
 use crate::api::console::Style;
 use crate::api::process::ExitCode;
-
-use alloc::string::ToString;
+use crate::api::unit::SizeUnit;
 
 pub fn main(args: &[&str]) -> Result<(), ExitCode> {
-    if args.len() == 1 || args[1] == "usage" {
-        usage();
-        Ok(())
-    } else if args[1] == "format" {
-        sys::fs::mount_mem();
-        sys::fs::format_mem();
-        println!("Memory successfully formatted");
-        println!("MFS is now mounted to '/'");
-        Ok(())
-    } else {
-        help();
-        Err(ExitCode::Failure)
+    match *args.get(1).unwrap_or(&"") {
+        "u" | "usage" => {
+            usage(&args[2..])
+        }
+        "f" | "format" => {
+            sys::fs::mount_mem();
+            sys::fs::format_mem();
+            println!("Memory successfully formatted");
+            println!("MFS is now mounted to '/'");
+            Ok(())
+        }
+        "-h" | "--help" => {
+            help();
+            Ok(())
+        }
+        _ => {
+            help();
+            Err(ExitCode::UsageError)
+        }
     }
 }
 
-fn usage() {
+fn usage(args: &[&str]) -> Result<(), ExitCode> {
+    let mut unit = SizeUnit::None;
+    for arg in args {
+        match *arg {
+            "-b" | "--binary-size" => {
+                unit = SizeUnit::Binary;
+            }
+            "-h" | "--help" => {
+                help_usage();
+                return Ok(());
+            }
+            _ => {
+                help_usage();
+                return Err(ExitCode::Failure);
+            }
+        }
+    }
     let size = sys::allocator::memory_size();
     let used = sys::allocator::memory_used();
     let free = size - used;
-
-    let width = size.to_string().len();
+    let width = [size, used, free].iter().fold(0, |acc, num| {
+        core::cmp::max(acc, unit.format(*num).len())
+    });
     let color = Style::color("LightCyan");
     let reset = Style::reset();
-    println!("{}size:{} {:width$} bytes", color, reset, size, width = width);
-    println!("{}used:{} {:width$} bytes", color, reset, used, width = width);
-    println!("{}free:{} {:width$} bytes", color, reset, free, width = width);
+    println!("{}size:{} {:width$}", color, reset, unit.format(size), width = width);
+    println!("{}used:{} {:width$}", color, reset, unit.format(used), width = width);
+    println!("{}free:{} {:width$}", color, reset, unit.format(free), width = width);
+    Ok(())
+}
+
+fn help_usage() {
+    let csi_option = Style::color("LightCyan");
+    let csi_title = Style::color("Yellow");
+    let csi_reset = Style::reset();
+    println!("{}Usage:{} memory usage {}<options>{}", csi_title, csi_reset, csi_option, csi_reset);
+    println!();
+    println!("{}Options:{}", csi_title, csi_reset);
+    println!("  {0}-b{1},{0} --binary-size{1}   Use binary size", csi_option, csi_reset);
 }
 
 fn help() {
