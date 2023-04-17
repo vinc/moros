b24ceffb2382af69cd86f934cf0fcbc0519dd6de
Vincent Ollivier <v@vinc.cc>
2023-04-17 10:26:16 +0200

    Add help to date command

--- src/usr/date.rs
+++ src/usr/date.rs
@@ -1,10 +1,17 @@
 use crate::api;
 use crate::api::clock::DATE_TIME_ZONE;
+use crate::api::console::Style;
 use crate::api::process::ExitCode;
 use time::validate_format_string;
 
 pub fn main(args: &[&str]) -> Result<(), ExitCode> {
+    if args.len() > 2 {
+        return Err(ExitCode::UsageError);
+    }
     let format = if args.len() > 1 { args[1] } else { DATE_TIME_ZONE };
+    if format == "-h" || format == "--help" {
+        return help();
+    }
     match validate_format_string(format) {
         Ok(()) => {
             println!("{}", api::time::now().format(format));
@@ -15,4 +22,12 @@ pub fn main(args: &[&str]) -> Result<(), ExitCode> {
             Err(ExitCode::Failure)
         }
     }
+}
+
+fn help() -> Result<(), ExitCode> {
+    let csi_option = Style::color("LightCyan");
+    let csi_title = Style::color("Yellow");
+    let csi_reset = Style::reset();
+    println!("{}Usage:{} date {}[<format>]{}", csi_title, csi_reset, csi_option, csi_reset);
+    Ok(())
 }
