99c730cea662dee78e47ee8cb86585b32cc2ff5f
Vincent Ollivier <v@vinc.cc>
2023-04-18 09:31:16 +0200

    Add help to move

--- src/usr/move.rs
+++ src/usr/move.rs
@@ -1,9 +1,21 @@
+use crate::api::console::Style;
 use crate::api::process::ExitCode;
 use crate::usr;
 
 pub fn main(args: &[&str]) -> Result<(), ExitCode> {
-    if args.len() != 3 {
+    let n = args.len();
+    if n != 3 {
+        help();
         return Err(ExitCode::UsageError);
+    }
+    for i in 1..n {
+        match args[i] {
+            "-h" | "--help" => {
+                help();
+                return Ok(());
+            }
+            _ => continue
+        }
     }
 
     // TODO: Avoid doing copy+delete
@@ -11,4 +23,11 @@ pub fn main(args: &[&str]) -> Result<(), ExitCode> {
         Ok(()) => usr::delete::main(&args[0..2]),
         _ => Err(ExitCode::Failure),
     }
+}
+
+fn help() {
+    let csi_option = Style::color("LightCyan");
+    let csi_title = Style::color("Yellow");
+    let csi_reset = Style::reset();
+    println!("{}Usage:{} move {}<src> <dst>{}", csi_title, csi_reset, csi_option, csi_reset);
 }
