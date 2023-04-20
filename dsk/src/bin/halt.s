4903c8fb00a2403ba0574c8e6cca0a44cf0342cf
Vincent Ollivier <v@vinc.cc>
2023-04-20 22:49:18 +0200

    Add sleep to halt

--- dsk/src/bin/halt.s
+++ dsk/src/bin/halt.s
@@ -1,16 +1,19 @@
 [bits 64]
 
 section .data
-msg: db 27, "[93m", "MOROS has reached its fate, the system is now halting.", 10, 27, "[0m"
+msg: db 27, "[93m", "MOROS has reached its fate, the system is now halting.", 27, "[0m", 10
 
 global _start
 section .text
 _start:
-  mov rax, 4                ; syscall number for WRITE
-  mov rdi, 1                ; standard output
-  mov rsi, msg              ; addr of string
-  mov rdx, 64               ; size of string
+  mov rax, 4                  ; syscall number for WRITE
+  mov rdi, 1                  ; standard output
+  mov rsi, msg                ; addr of string
+  mov rdx, 64                 ; size of string
   int 0x80
-  mov rax, 0xa              ; syscall number for STOP
-  mov rdi, 0xdead           ; halt code
+  mov rax, 0xb                ; syscall number for SLEEP
+  mov rdi, __?float64?__(0.5) ; duration
+  int 0x80
+  mov rax, 0xa                ; syscall number for STOP
+  mov rdi, 0xdead             ; halt code
   int 0x80
