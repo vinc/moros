86801b16a240d706cced61e7eea3f984054fe761
Vincent Ollivier <v@vinc.cc>
2023-04-20 22:52:10 +0200

    Add unimplemented print and sleep

---
+++ dsk/src/bin/sleep.s
@@ -0,0 +1,16 @@
+[bits 64]
+
+section .data
+msg: db "Not implemented", 10
+
+global _start
+section .text
+_start:
+  mov rax, 4                ; syscall number for WRITE
+  mov rdi, 1                ; standard output
+  mov rsi, msg              ; addr of string
+  mov rdx, 16               ; size of string
+  int 0x80
+  mov rax, 1                ; syscall number for EXIT
+  mov rdi, 0                ; no error
+  int 0x80
