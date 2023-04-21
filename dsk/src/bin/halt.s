ab21a8e6a0f9dd5c348d1f34891f4a87d6565e9c
Vincent Ollivier <v@vinc.cc>
2023-04-21 19:52:42 +0200

    Compute msg len

--- dsk/src/bin/halt.s
+++ dsk/src/bin/halt.s
@@ -2,6 +2,7 @@
 
 section .data
 msg: db 27, "[93m", "MOROS has reached its fate, the system is now halting.", 27, "[0m", 10
+len: equ $-msg
 
 global _start
 section .text
@@ -9,11 +10,13 @@ _start:
   mov rax, 4                  ; syscall number for WRITE
   mov rdi, 1                  ; standard output
   mov rsi, msg                ; addr of string
-  mov rdx, 64                 ; size of string
+  mov rdx, len                ; size of string
   int 0x80
+
   mov rax, 0xb                ; syscall number for SLEEP
   mov rdi, __?float64?__(0.5) ; duration
   int 0x80
+
   mov rax, 0xa                ; syscall number for STOP
   mov rdi, 0xdead             ; halt code
   int 0x80
