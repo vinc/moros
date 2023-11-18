90c1662427d521049492324d20e5b1b76bc92e4c
Vincent Ollivier <v@vinc.cc>
2023-11-18 10:21:44 +0100

    Fix demo

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -13,8 +13,11 @@ entry_point!(main);
 
 fn main(args: &[&str]) {
     if args.len() > 1 {
+        syscall::write(1, args[1].as_bytes());    // FIXME: this is needed
+        syscall::write(1, "\n".as_bytes());
+
         let mut hello = "Hello, ".to_string();
-        hello.push_str(args[1]);
+        hello.push_str(args[1]);                  // FIXME: for that to work
         hello.push_str("!\n");
         syscall::write(1, hello.as_bytes());
     } else {
