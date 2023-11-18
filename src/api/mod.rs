8d4da3e8dc179c01df995f699c43ab749c08a50f
Vincent Ollivier <v@vinc.cc>
2023-11-18 18:37:41 +0100

    Return from userspace exceptions

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -5,6 +5,7 @@ macro_rules! entry_point {
         #[panic_handler]
         fn panic(_info: &core::panic::PanicInfo) -> ! {
             $crate::api::syscall::write(1, b"An exception occured!\n");
+            $crate::api::syscall::exit($crate::api::process::ExitCode::ExecError);
             loop {}
         }
 
