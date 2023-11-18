011de026c1fe8474b031271743ec68cf32ce6412
Vincent Ollivier <v@vinc.cc>
2023-11-18 18:38:06 +0100

    Add failing test

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -3,23 +3,28 @@
 
 extern crate alloc;
 
-use alloc::format;
-use alloc::string::String;
 use alloc::string::ToString;
-use moros::api::{allocator, syscall};
+use moros::api::syscall;
 use moros::entry_point;
 
 entry_point!(main);
 
 fn main(args: &[&str]) {
     if args.len() > 1 {
-        syscall::write(1, args[1].as_bytes());    // FIXME: this is needed
+        syscall::write(1, args[1].as_bytes());      // FIXME: this is needed
         syscall::write(1, "\n".as_bytes());
 
         let mut hello = "Hello, ".to_string();
-        hello.push_str(args[1]);                  // FIXME: for that to work
+        hello.push_str(args[1]);                    // FIXME: for that to work
         hello.push_str("!\n");
         syscall::write(1, hello.as_bytes());
+
+        if args.len() > 2 {
+            let mut hello = "Hello, ".to_string();
+            hello.push_str(args[2]);                // FIXME: not working
+            hello.push_str("!\n");
+            syscall::write(1, hello.as_bytes());
+        }
     } else {
         syscall::write(1, b"Hello, World!\n");
     }
