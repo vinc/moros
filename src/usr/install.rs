6621bc69410b40a65db3a0b394f331b1c619412b
Vincent Ollivier <v@vinc.cc>
2023-11-19 16:11:11 +0100

    Fix dsk/var/www

--- src/usr/install.rs
+++ src/usr/install.rs
@@ -89,8 +89,8 @@ pub fn copy_files(verbose: bool) {
 
     create_dir("/var/www", verbose);
     copy_file("/var/www/index.html", include_bytes!("../../dsk/var/www/index.html"), verbose);
-    copy_file("/var/www/moros.png", include_bytes!("../../dsk/var/www/moros.png"), verbose);
     copy_file("/var/www/moros.css", include_bytes!("../../dsk/var/www/moros.css"), verbose);
+    copy_file("/var/www/moros.png", include_bytes!("../../dsk/var/www/moros.png"), verbose);
 }
 
 pub fn main(args: &[&str]) -> Result<(), ExitCode> {
