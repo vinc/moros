d02032e9bec74bdb3e92d37d06053814b40e37c3
Vincent Ollivier <v@vinc.cc>
2023-11-20 20:53:09 +0100

    Add exec command

---
+++ src/bin/exec.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use alloc::string::String;
+use alloc::vec;
+use moros::api::io;
+use moros::api::syscall;
+use moros::api::process;
+use moros::entry_point;
+
+entry_point!(main);
+
+fn main(_args: &[&str]) {
+    loop {
+        syscall::write(1, "\n> ".as_bytes());
+        let line = io::stdin().read_line();
+        let cmd = line.trim();
+        if cmd == "quit" {
+            syscall::exit(process::ExitCode::Success);
+        } else {
+            let args = vec![];
+            let mut path = String::from("/bin/");
+            path.push_str(cmd);
+            process::spawn(&path, &args);
+        }
+    }
+}
