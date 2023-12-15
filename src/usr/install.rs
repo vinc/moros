b26d937cedd53c72cf8ce3900d0b78582c294417
Vincent Ollivier <v@vinc.cc>
2023-12-15 19:21:11 +0100

    Add doc.lsp

--- src/usr/install.rs
+++ src/usr/install.rs
@@ -65,6 +65,7 @@ pub fn copy_files(verbose: bool) {
 
     create_dir("/tmp/lisp", verbose);
     copy_file("/tmp/lisp/colors.lsp", include_bytes!("../../dsk/tmp/lisp/colors.lsp"), verbose);
+    copy_file("/tmp/lisp/doc.lsp", include_bytes!("../../dsk/tmp/lisp/doc.lsp"), verbose);
     copy_file("/tmp/lisp/factorial.lsp", include_bytes!("../../dsk/tmp/lisp/factorial.lsp"), verbose);
     //copy_file("/tmp/lisp/fetch.lsp", include_bytes!("../../dsk/tmp/lisp/fetch.lsp"), verbose);
     copy_file("/tmp/lisp/fibonacci.lsp", include_bytes!("../../dsk/tmp/lisp/fibonacci.lsp"), verbose);
