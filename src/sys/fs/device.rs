b8bd3633d98cc832a435cd9dbed7179f19300c9a
Vincent Ollivier <v@vinc.cc>
2024-03-15 12:34:29 +0100

    Refactor long lines

--- src/sys/fs/device.rs
+++ src/sys/fs/device.rs
@@ -35,17 +35,17 @@ impl TryFrom<&[u8]> for DeviceType {
     type Error = ();
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        match buf.get(0) {
-            Some(i) if *i == DeviceType::Null      as u8 => Ok(DeviceType::Null),
-            Some(i) if *i == DeviceType::File      as u8 => Ok(DeviceType::File),
-            Some(i) if *i == DeviceType::Console   as u8 => Ok(DeviceType::Console),
-            Some(i) if *i == DeviceType::Random    as u8 => Ok(DeviceType::Random),
-            Some(i) if *i == DeviceType::Uptime    as u8 => Ok(DeviceType::Uptime),
-            Some(i) if *i == DeviceType::Realtime  as u8 => Ok(DeviceType::Realtime),
-            Some(i) if *i == DeviceType::RTC       as u8 => Ok(DeviceType::RTC),
-            Some(i) if *i == DeviceType::TcpSocket as u8 => Ok(DeviceType::TcpSocket),
-            Some(i) if *i == DeviceType::UdpSocket as u8 => Ok(DeviceType::UdpSocket),
-            Some(i) if *i == DeviceType::Drive     as u8 => Ok(DeviceType::Drive),
+        match buf.get(0).ok_or(())? {
+            0 => Ok(DeviceType::Null),
+            1 => Ok(DeviceType::File),
+            2 => Ok(DeviceType::Console),
+            3 => Ok(DeviceType::Random),
+            4 => Ok(DeviceType::Uptime),
+            5 => Ok(DeviceType::Realtime),
+            6 => Ok(DeviceType::RTC),
+            7 => Ok(DeviceType::TcpSocket),
+            8 => Ok(DeviceType::UdpSocket),
+            9 => Ok(DeviceType::Drive),
             _ => Err(()),
         }
     }
