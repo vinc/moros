597327324db5bba25ce14292d101fd01ec7a43b9
Vincent Ollivier <v@vinc.cc>
2024-03-18 21:35:30 +0100

    Add RDRAND retry

--- src/sys/random.rs
+++ src/sys/random.rs
@@ -39,13 +39,24 @@ impl FileIO for Random {
 
 pub fn get_u64() -> u64 {
     let mut seed = [0u8; 32];
+    // NOTE: Intel's Software Developer's Manual, Volume 1, 7.3.17.1
     if let Some(rdrand) = RdRand::new() {
         for i in 0..4 {
-            if let Some(rand) = rdrand.get_u64() {
-                let bytes = rand.to_be_bytes();
-                for j in 0..8 {
-                    seed[8 * i + j] = bytes[j];
+            let mut success = false;
+            for _ in 0..10 { // Retry up to 10 times
+                if let Some(rand) = rdrand.get_u64() {
+                    let bytes = rand.to_be_bytes();
+                    for j in 0..8 {
+                        seed[8 * i + j] = bytes[j];
+                    }
+                    success = true;
+                    break;
+                } else {
+                    //debug!("RDRAND: read failed");
                 }
+            }
+            if !success {
+                //debug!("RDRAND: retry failed");
             }
         }
     } else {
