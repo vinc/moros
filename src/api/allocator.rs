6cfcaaccd2404d527d98233099fec23ff7483862
Vincent Ollivier <v@vinc.cc>
2024-09-28 16:39:08 +0200

    Add special alloc_error_handler for userspace (#673)

--- src/api/allocator.rs
+++ src/api/allocator.rs
@@ -1,3 +1,5 @@
+use crate::hlt_loop;
+use crate::api::process::ExitCode;
 use crate::api::syscall;
 
 use core::alloc::{GlobalAlloc, Layout};
@@ -17,3 +19,11 @@ unsafe impl GlobalAlloc for UserspaceAllocator {
 #[allow(dead_code)]
 #[cfg_attr(feature = "userspace", global_allocator)]
 static ALLOCATOR: UserspaceAllocator = UserspaceAllocator;
+
+#[allow(dead_code)]
+#[cfg_attr(feature = "userspace", alloc_error_handler)]
+fn alloc_error_handler(_layout: alloc::alloc::Layout) -> ! {
+    syscall::write(2, b"\x1b[91mError:\x1b[m Could not allocate\n");
+    syscall::exit(ExitCode::PageFaultError);
+    hlt_loop();
+}
