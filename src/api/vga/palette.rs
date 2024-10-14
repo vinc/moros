d24ab74e6609d0d24ddabd1d7bddb1b376147982
Vincent Ollivier <v@vinc.cc>
2024-10-14 19:52:51 +0200

    Remove cvs support for palette

--- src/api/vga/palette.rs
+++ src/api/vga/palette.rs
@@ -1,6 +1,3 @@
-use alloc::vec::Vec;
-use core::convert::TryInto;
-
 // TODO: Move this to kernel after removing the `vga set palette` command
 pub struct Palette {
     pub colors: [(u8, u8, u8); 16],
@@ -29,37 +26,4 @@ impl Palette {
             ],
         }
     }
-}
-
-// TODO: Remove this
-pub fn from_csv(s: &str) -> Result<Palette, ()> {
-    let colors: Vec<_> = s.lines().filter_map(|line| {
-        let line = line.split('#').next().unwrap(); // Remove comments
-        let color: Vec<u8> = line.split(',').filter_map(|value| {
-            let radix = if value.contains("0x") { 16 } else { 10 };
-            let value = value.trim().trim_start_matches("0x");
-            u8::from_str_radix(value, radix).ok()
-        }).collect();
-        if color.len() == 3 { // RGB values
-            Some((color[0], color[1], color[2]))
-        } else {
-            None
-        }
-    }).collect();
-    if let Ok(colors) = colors.try_into() { // Array of 16 colors
-        Ok(Palette { colors })
-    } else {
-        Err(())
-    }
-}
-
-#[test_case]
-fn test_from_csv() {
-    assert!(from_csv("").is_err());
-    assert!(from_csv("0,0,0,0").is_err());
-
-    let s = include_str!("../../../dsk/ini/palettes/gruvbox-dark.csv");
-    let palette = from_csv(s).unwrap();
-    assert_eq!(palette.colors[0x03].0, 0x68);
-    assert_eq!(palette.colors[0x0D].1, 0x86);
 }
