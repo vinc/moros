45eeb99ccd153e07a6df678d68adeee5ae3965cb
Vincent Ollivier <v@vinc.cc>
2024-11-03 15:52:12 +0100

    Fix BMP error messages

--- src/usr/render.rs
+++ src/usr/render.rs
@@ -51,18 +51,18 @@ fn parse_bmp(data: &[u8]) -> Result<BmpInfo, String> {
 
     let bmp_header: &BmpHeader = unsafe { &*(data.as_ptr() as *const BmpHeader) };
     if &bmp_header.signature != b"BM" {
-        return Err("Invalid BMP signature".to_string());
+        return Err("Invalid signature".to_string());
     }
 
     let dib_header: &DibHeader = unsafe { &*(data[size_of::<BmpHeader>()..].as_ptr() as *const DibHeader) };
     if dib_header.bits_per_pixel != 8 {
-        return Err("Only 8-bit (256 color) BMPs are supported".to_string());
+        return Err("Invalid bits per pixel".to_string());
     }
 
     let pixels_offset = bmp_header.data_offset as usize;
     let palette_size = 256 * 4; // 256 colors, 4 bytes per color (BGRA)
     if pixels_offset < palette_size || data.len() <= pixels_offset {
-        return Err("Invalid BMP palette".to_string());
+        return Err("Invalid palette".to_string());
     }
     let palette_offset = pixels_offset - palette_size;
 
@@ -76,7 +76,7 @@ fn parse_bmp(data: &[u8]) -> Result<BmpInfo, String> {
     let width = dib_header.width as u32;
     let height = dib_header.height.unsigned_abs();
     if pixels.len() != (width * height) as usize {
-        return Err("Invalid BMP file: wrong pixels count".to_string());
+        return Err("Invalid pixels count".to_string());
     }
 
     Ok(BmpInfo { width, height, palette, pixels })
@@ -131,59 +131,62 @@ impl Config {
 
 fn render_bmp(path: &str, config: &mut Config) -> Result<Command, ExitCode> {
     if let Ok(buf) = fs::read_to_bytes(path) {
-        if let Ok(bmp) = parse_bmp(&buf) {
-            let width = bmp.width as usize;
-            let height = bmp.height as usize;
-            if width != WIDTH || height != HEIGHT {
-                config.text_mode();
-                error!("Unsupported BMP size");
-                return Err(ExitCode::Failure);
-            }
-            let size = width * height;
-            let mut img = Vec::with_capacity(size);
-
-            // BMP rows are padded to multiples of 4 bytes
-            let row_padding = (4 - (width % 4)) % 4;
-
-            for y in 0..height {
-                for x in 0..width {
-                    // BMP stores images bottom-up
-                    let bmp_y = height - 1 - y;
-
-                    let i = bmp_y * (width + row_padding) + x;
-                    img.push(bmp.pixels[i]);
+        match parse_bmp(&buf) {
+            Ok(bmp) => {
+                let width = bmp.width as usize;
+                let height = bmp.height as usize;
+                if width != WIDTH || height != HEIGHT {
+                    config.text_mode();
+                    error!("Unsupported BMP size");
+                    return Err(ExitCode::Failure);
                 }
-            }
+                let size = width * height;
+                let mut img = Vec::with_capacity(size);
 
-            config.graphic_mode();
+                // BMP rows are padded to multiples of 4 bytes
+                let row_padding = (4 - (width % 4)) % 4;
 
-            // Load palette
-            let mut palette = [0; 256 * 3];
-            for (i, (r, g, b)) in bmp.palette.iter().enumerate() {
-                palette[i * 3 + 0] = *r;
-                palette[i * 3 + 1] = *g;
-                palette[i * 3 + 2] = *b;
+                for y in 0..height {
+                    for x in 0..width {
+                        // BMP stores images bottom-up
+                        let bmp_y = height - 1 - y;
+
+                        let i = bmp_y * (width + row_padding) + x;
+                        img.push(bmp.pixels[i]);
+                    }
+                }
+
+                config.graphic_mode();
+
+                // Load palette
+                let mut palette = [0; 256 * 3];
+                for (i, (r, g, b)) in bmp.palette.iter().enumerate() {
+                    palette[i * 3 + 0] = *r;
+                    palette[i * 3 + 1] = *g;
+                    palette[i * 3 + 2] = *b;
+                }
+                let dev = "/dev/vga/palette";
+                if !fs::is_device(dev) || fs::write(dev, &palette).is_err() {
+                    config.text_mode();
+                    error!("Could not write to '{}'", dev);
+                    return Err(ExitCode::Failure);
+                }
+
+                // Display image
+                let dev = "/dev/vga/buffer";
+                if !fs::is_device(dev) || fs::write(dev, &img).is_err() {
+                    config.text_mode();
+                    error!("Could not write to '{}'", dev);
+                    return Err(ExitCode::Failure);
+                }
+
+                Ok(read_command())
             }
-            let dev = "/dev/vga/palette";
-            if !fs::is_device(dev) || fs::write(dev, &palette).is_err() {
+            Err(msg) => {
                 config.text_mode();
-                error!("Could not write to '{}'", dev);
-                return Err(ExitCode::Failure);
+                error!("{}", msg);
+                Err(ExitCode::Failure)
             }
-
-            // Display image
-            let dev = "/dev/vga/buffer";
-            if !fs::is_device(dev) || fs::write(dev, &img).is_err() {
-                config.text_mode();
-                error!("Could not write to '{}'", dev);
-                return Err(ExitCode::Failure);
-            }
-
-            Ok(read_command())
-        } else {
-            config.text_mode();
-            error!("Could not parse BMP");
-            Err(ExitCode::Failure)
         }
     } else {
         config.text_mode();
