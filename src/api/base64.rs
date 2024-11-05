8cc3e2710da4289b971f110b44c4c960594686b4
Vincent Ollivier <v@vinc.cc>
2024-11-05 12:42:40 +0100

    Rewrite base64 code

--- src/api/base64.rs
+++ src/api/base64.rs
@@ -1,26 +1,15 @@
 use alloc::vec::Vec;
+use base64::prelude::*;
 
 pub struct Base64;
 
 impl Base64 {
     pub fn encode(s: &[u8]) -> Vec<u8> {
-        let mut buf: Vec<u8> = Vec::new();
-        buf.resize(s.len() * 4 / 3 + 4, 0); // Resize to base64 + padding
-        let bytes_written = base64::encode_config_slice(
-            s, base64::STANDARD_NO_PAD, &mut buf
-        );
-        buf.resize(bytes_written, 0); // Resize back to actual size
-        buf
+        BASE64_STANDARD_NO_PAD.encode(s).as_bytes().to_vec()
     }
 
     pub fn decode(s: &[u8]) -> Result<Vec<u8>, ()> {
-        let mut buf: Vec<u8> = Vec::new();
-        buf.resize(s.len(), 0);
-        let bytes_written = base64::decode_config_slice(
-            s, base64::STANDARD_NO_PAD, &mut buf
-        ).map_err(|_| ())?;
-        buf.resize(bytes_written, 0);
-        Ok(buf)
+        BASE64_STANDARD_NO_PAD.decode(s).map_err(|_| ())
     }
 }
 
