7e331592b9a4aca70696a2c87e897b00971d4271
Vincent Ollivier <v@vinc.cc>
2024-11-05 12:50:58 +0100

    Upgrade base64 module (#699)
    * Improve test coverage
    
    * Rewrite base64 code

--- src/api/base64.rs
+++ src/api/base64.rs
@@ -1,33 +1,27 @@
 use alloc::vec::Vec;
+use base64::prelude::*;
 
 pub struct Base64;
 
 impl Base64 {
     pub fn encode(s: &[u8]) -> Vec<u8> {
-        let mut buf: Vec<u8> = Vec::new();
-        buf.resize(s.len() * 4 / 3 + 4, 0); // Resize to base64 + padding
-        let bytes_written = base64::encode_config_slice(
-            s, base64::STANDARD_NO_PAD, &mut buf
-        );
-        buf.resize(bytes_written, 0); // Resize back to actual size
-        buf
+        BASE64_STANDARD_NO_PAD.encode(s).as_bytes().to_vec()
     }
 
     pub fn decode(s: &[u8]) -> Result<Vec<u8>, ()> {
-        let mut buf: Vec<u8> = Vec::new();
-        buf.resize(s.len(), 0);
-        let bytes_written = base64::decode_config_slice(
-            s, base64::STANDARD_NO_PAD, &mut buf
-        ).map_err(|_| ())?;
-        buf.resize(bytes_written, 0);
-        Ok(buf)
+        BASE64_STANDARD_NO_PAD.decode(s).map_err(|_| ())
     }
 }
 
 #[test_case]
 fn test_base64() {
-    let decoded = b"Hello, World!";
-    let encoded = b"SGVsbG8sIFdvcmxkIQ";
-    assert_eq!(Base64::encode(decoded), encoded.to_vec());
-    assert_eq!(Base64::decode(encoded), Ok(decoded.to_vec()));
+    let tests = [
+        (b"abcdefghijklm", b"YWJjZGVmZ2hpamtsbQ"),
+        (b"Hello, World!", b"SGVsbG8sIFdvcmxkIQ"),
+        (b"~~~~~, ?????!", b"fn5+fn4sID8/Pz8/IQ"),
+    ];
+    for (decoded, encoded) in tests {
+        assert_eq!(Base64::encode(decoded), encoded.to_vec());
+        assert_eq!(Base64::decode(encoded), Ok(decoded.to_vec()));
+    }
 }
