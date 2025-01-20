4e11265693d6266eb5f2323b1ef022bfdd4b6432
Pedro Vinicius Carvalho Ferreira dos Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2025-01-20 17:56:41 -0300

    Bump pc-keyboard from 0.6.1 to 0.8.0

--- src/sys/keyboard.rs
+++ src/sys/keyboard.rs
@@ -40,13 +40,13 @@ impl KeyboardLayout {
 
     fn from(name: &str) -> Option<Self> {
         match name {
-            "azerty" => Some(KeyboardLayout::Azerty(Keyboard::new(
+            "azerty" => Some(KeyboardLayout::Azerty(Keyboard::new(ScancodeSet1::new(), layouts::Azerty,
                 HandleControl::MapLettersToUnicode,
             ))),
-            "dvorak" => Some(KeyboardLayout::Dvorak(Keyboard::new(
+            "dvorak" => Some(KeyboardLayout::Dvorak(Keyboard::new(ScancodeSet1::new(), layouts::Dvorak104Key,
                 HandleControl::MapLettersToUnicode,
             ))),
-            "qwerty" => Some(KeyboardLayout::Qwerty(Keyboard::new(
+            "qwerty" => Some(KeyboardLayout::Qwerty(Keyboard::new(ScancodeSet1::new(), layouts::Us104Key,
                 HandleControl::MapLettersToUnicode,
             ))),
             _ => None,
@@ -91,13 +91,13 @@ fn interrupt_handler() {
         if let Ok(Some(event)) = keyboard.add_byte(scancode) {
             let ord = Ordering::Relaxed;
             match event.code {
-                KeyCode::AltLeft | KeyCode::AltRight => {
+                KeyCode::LAlt | KeyCode::RAltGr => {
                     ALT.store(event.state == KeyState::Down, ord)
                 }
-                KeyCode::ShiftLeft | KeyCode::ShiftRight => {
+                KeyCode::LShift | KeyCode::RShift => {
                     SHIFT.store(event.state == KeyState::Down, ord)
                 }
-                KeyCode::ControlLeft | KeyCode::ControlRight => {
+                KeyCode::LControl | KeyCode::RControl => {
                     CTRL.store(event.state == KeyState::Down, ord)
                 }
                 _ => {}
