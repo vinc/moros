63cc7f5bf26567dc4447e5c95c5ffcff5d21d48c
Vincent Ollivier <v@vinc.cc>
2025-07-05 14:38:52 +0200

    Add comment

--- src/usr/httpd.rs
+++ src/usr/httpd.rs
@@ -368,7 +368,7 @@ pub fn main(args: &[&str]) -> Result<(), ExitCode> {
 
                 if !socket.is_open() {
                     socket.listen(port).unwrap();
-                    *keep_alive = true;
+                    *keep_alive = true; // Reset to default
                 }
                 let endpoint = match socket.remote_endpoint() {
                     Some(endpoint) => endpoint,
