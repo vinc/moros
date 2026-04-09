312612db98a9d809eccc23a3c3a0af918aafeee4
Vincent Ollivier <v@vinc.cc>
2026-04-09 21:33:08 +0200

    Rename /dev/snd/buf to /dev/snd/buffer

--- src/usr/install.rs
+++ src/usr/install.rs
@@ -69,7 +69,7 @@ pub fn copy_files(verbose: bool) {
     create_dev("/dev/null", "null", verbose);
     create_dev("/dev/pipe", "pipe", verbose);
     create_dev("/dev/random", "random", verbose);
-    create_dev("/dev/snd/buf", "snd-buffer", verbose);
+    create_dev("/dev/snd/buffer", "snd-buffer", verbose);
     create_dev("/dev/speaker", "speaker", verbose);
     create_dev("/dev/vga/buffer", "vga-buffer", verbose);
     create_dev("/dev/vga/font", "vga-font", verbose);
