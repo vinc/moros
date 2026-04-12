56aeb6a79fc8c61564fc9bdc06fb6957da24419e
Vincent Ollivier <v@vinc.cc>
2026-04-12 02:14:18 +0200

    Update device struct

--- src/sys/snd/ac97.rs
+++ src/sys/snd/ac97.rs
@@ -6,11 +6,14 @@ use crate::sys::mem::PhysBuf;
 
 use alloc::vec::Vec;
 use alloc::sync::Arc;
+use core::sync::atomic::{AtomicUsize, Ordering};
 use core::ptr;
 use spin::Mutex;
 
 // Sources:
 // https://wiki.osdev.org/AC97
+
+const BDL: usize = 32;
 
 #[derive(Clone, Copy, Debug, Default)]
 #[repr(C, align(16))]
@@ -22,12 +25,13 @@ struct BufDesc {
 
 pub struct Device {
     is_playing: bool,
-    buffer: Vec<u8>,
     config: SoundConfig,
-    block: PhysBuf,
+    buffer: Vec<u8>,
+    blocks: [PhysBuf; BDL],
+    index: Arc<AtomicUsize>,
+    bdl: Arc<Mutex<[BufDesc; BDL]>>, // Buffer Descriptor List
     bar0: u16,
     bar1: u16,
-    bdl: Arc<Mutex<[BufDesc; 32]>>, // Buffer Descriptor List
 }
 
 impl Device {
@@ -37,50 +41,56 @@ impl Device {
         sys::clk::wait(100_000); // 100ms
         outw(bar0 + 0x00, 1); // Reset Register
         sys::clk::wait(100_000); // 100ms
-        println!("SND AC97 RC: {:#016b}", inw(bar0 + 0x00)); // Capabilities
-        println!("SND AC97 EC: {:#016b}", inw(bar0 + 0x28)); // Ext Cap
-        println!("SND AC97 GS: {:#032b}", inl(bar1 + 0x30)); // Global Status
+        debug!("SND AC97 RC: {:#016b}", inw(bar0 + 0x00)); // Capabilities
+        debug!("SND AC97 EC: {:#016b}", inw(bar0 + 0x28)); // Ext Cap
+        debug!("SND AC97 GS: {:#032b}", inl(bar1 + 0x30)); // Global Status
 
         Self {
             is_playing: false,
-            buffer: Vec::new(),
             config: SoundConfig::new(),
-            block: PhysBuf::new(SoundBuffer::size()),
-            bdl: Arc::new(Mutex::new([(); 32].map(|_| BufDesc::default()))),
+            buffer: Vec::new(),
+            blocks: [(); BDL].map(|_| PhysBuf::new(SoundBuffer::size())),
+            index: Arc::new(AtomicUsize::new(0)),
+            bdl: Arc::new(Mutex::new([(); BDL].map(|_| BufDesc::default()))),
             bar0, bar1
         }
     }
 
     pub fn play(&mut self, buffer: &[u8], config: &SoundConfig) {
+        //self.buffer.extend_from_slice(buffer);
+
         debug!("play(buf.len={}, {:?})", buffer.len(), config);
-        println!("SND AC97 Output Channel: {:#08b}", inb(self.bar1 + 0x1B));
+        debug!("SND AC97 Output Channel: {:#08b}", inb(self.bar1 + 0x1B));
         outb(self.bar1 + 0x1B, 0x02); // Set reset bit of output channel
         while inb(self.bar1 + 0x1B) & 0x02 != 0 {
             // Wait for reset to be completed
         }
-        println!("SND AC97 Output Channel: {:#08b}", inb(self.bar1 + 0x1B));
+        debug!("SND AC97 Output Channel: {:#08b}", inb(self.bar1 + 0x1B));
 
         outw(self.bar0 + 0x2A, 1);
         outw(self.bar0 + 0x2C, config.sample_rate as u16);
-        println!("SND AC97 Sample Rate: {} Hz", inw(self.bar0 + 0x2C));
+        debug!("SND AC97 Sample Rate: {} Hz", inw(self.bar0 + 0x2C));
 
         // Write physical position of BDL to Buffer Descriptor Base Address
         // register
         let mut bdl = self.bdl.lock();
 
-        let n = core::cmp::min(self.block.len(), buffer.len());
-        self.block[0..n].copy_from_slice(&buffer[0..n]);
+        let i = self.index.update(Ordering::SeqCst, Ordering::SeqCst, |i| {
+            (i + 1) % BDL
+        });
+        let n = core::cmp::min(self.blocks[i].len(), buffer.len());
+        self.blocks[i][0..n].copy_from_slice(&buffer[0..n]);
 
-        bdl[0].addr = self.block.addr() as u32;
-        bdl[0].size = (n / 4) as u16;
-        bdl[0].ctrl = 1 << 15; // IOC: Interrupt on Completion
+        bdl[i].addr = self.blocks[i].addr() as u32;
+        bdl[i].size = (n / 4) as u16;
+        bdl[i].ctrl = 1 << 15; // IOC: Interrupt on Completion
 
-        let ptr = ptr::addr_of!(bdl[0]) as *const u8;
+        let ptr = ptr::addr_of!(bdl[i]) as *const u8;
         let addr = sys::mem::phys_addr(ptr);
         outl(self.bar1 + 0x10, addr as u32);
 
         // Write number of last valid buffer entry to Last Valid Entry register
-        outb(self.bar1 + 0x15, 0);
+        outb(self.bar1 + 0x15, i as u8);
 
         // Start DMA
         outb(self.bar1 + 0x1B, 0x01);
