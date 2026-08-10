99e7f32069b27a64d859528be6ac21ae2c0138b1
Vincent Ollivier <v@vinc.cc>
2026-08-10 19:11:32 +0200

    Remove dependency on pic8259 crate

--- src/sys/pic.rs
+++ src/sys/pic.rs
@@ -1,18 +1,22 @@
 use crate::sys::x86::port::*;
 
-use pic8259::ChainedPics;
-use spin::Mutex;
-
+// Ports
 const PIC1_CMD: u16 = 0x20;
 const PIC2_CMD: u16 = 0xA0;
 const PIC1_DATA: u16 = PIC1_CMD + 1;
 const PIC2_DATA: u16 = PIC2_CMD + 1;
-const PIC1_OFFSET: u8 = 32;
-const PIC2_OFFSET: u8 = PIC1_OFFSET + 8;
 
-static PICS: Mutex<ChainedPics> = Mutex::new(unsafe {
-    ChainedPics::new(PIC1_OFFSET, PIC2_OFFSET)
-});
+// Vector offset
+const PIC_OFFSET: u8 = 0x20;
+
+// Initialization Command Words (ICW)
+const ICW1_INIT: u8 = 0x10; // Init (A0=0, D4=1)
+const ICW1_ICW4: u8 = 0x01; // ICW4 needed (D0=1)
+const ICW4_8086: u8 = 0x01; // 8086 mode (D0=1)
+
+// Operation Command Words (OCW)
+const PIC_EOI: u8 = 0x20; // End-of-interrupt (D5=1)
+
 // IRQ List
 pub const PIT_IRQ: u8 = 0;
 pub const KBD_IRQ: u8 = 1;
@@ -46,18 +50,65 @@ pub fn unmask(irq: u8) {
 }
 
 pub fn vector(irq: u8) -> u8 {
-    PIC1_OFFSET + irq
+    PIC_OFFSET + irq
 }
 
 pub fn eoi(irq: u8) {
-    let i = vector(irq);
     unsafe {
-        PICS.lock().notify_end_of_interrupt(i)
+        if irq >= 8 {
+            outb(PIC2_CMD, PIC_EOI);
+        }
+        outb(PIC1_CMD, PIC_EOI);
+    }
+}
+
+fn wait() {
+    unsafe {
+        outb(0x80, 0);
+    }
+}
+
+fn mask_all() {
+    unsafe {
+        outb(PIC1_DATA, 0xFF);
+        outb(PIC2_DATA, 0xFF);
+    }
+}
+
+fn remap(offset: u8) {
+    unsafe {
+        // Init
+        outb(PIC1_CMD, ICW1_INIT | ICW1_ICW4); // Clears the mask register
+        wait();
+        outb(PIC2_CMD, ICW1_INIT | ICW1_ICW4); // Clears the mask register
+        wait();
+
+        // Vector
+        outb(PIC1_DATA, offset);
+        wait();
+        outb(PIC2_DATA, offset + 8);
+        wait();
+
+        // Cascade
+        outb(PIC1_DATA, 1 << PIC_IRQ);
+        wait();
+        outb(PIC2_DATA, PIC_IRQ);
+        wait();
+
+        // Mode
+        outb(PIC1_DATA, ICW4_8086);
+        wait();
+        outb(PIC2_DATA, ICW4_8086);
     }
 }
 
 pub fn init() {
-    unsafe {
-        PICS.lock().initialize();
-    }
+    // Initially PIC1 has an offset of 0x8 which overlaps with processor
+    // interrupts so we need to map them higher, but below the system call
+    // interrupt at 0x80.
+    remap(PIC_OFFSET);
+
+    // The remapping unmasked everything
+    mask_all();
+    unmask(PIC_IRQ);
 }
