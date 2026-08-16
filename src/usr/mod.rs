af7f854014d10ab2262f550fb86596bc69a5a169
Vincent Ollivier <v@vinc.cc>
2026-08-16 14:52:35 +0200

    Remove old geodate command

--- src/usr/mod.rs
+++ src/usr/mod.rs
@@ -14,7 +14,6 @@ pub mod edit;
 pub mod elf;
 pub mod encode;
 pub mod find;
-//pub mod geodate; // TODO: Remove file
 pub mod hash;
 pub mod help;
 pub mod hex;
--- src/usr/shell.rs
+++ src/usr/shell.rs
@@ -608,7 +608,6 @@ fn dispatch(args: &[&str], config: &mut Config) -> Result<(), ExitCode> {
         "elf"       => usr::elf::main(args),
         "encode"    => usr::encode::main(args),
         "find"      => usr::find::main(args),
-        //"geodate"   => usr::geodate::main(args),
         "goto"      => cmd_change_dir(args, config), // TODO: Remove this
         "hash"      => usr::hash::main(args),
         "help"      => usr::help::main(args),
