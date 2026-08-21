39875a952f3eebb424e615fa56b447579412e0d7
Vincent Ollivier <v@vinc.cc>
2026-08-22 01:07:23 +0200

    Add TaskStateSegment

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -58,5 +58,6 @@ pub mod serial;
 #[cfg(target_arch = "x86_64")] pub mod snd;
 #[cfg(target_arch = "x86_64")] pub mod speaker;
 #[cfg(target_arch = "x86_64")] pub mod syscall;
+pub mod tss;
 #[cfg(target_arch = "x86_64")] pub mod vga;
 pub mod x86;
