e36849f4a461067b519b56ba143917ee4e4119b5
Vincent Ollivier <v@vinc.cc>
2026-08-25 21:00:26 +0200

    Update changelog script

--- run/update-changelog.sh
+++ run/update-changelog.sh
@@ -2,3 +2,5 @@
 title="$(gh pr view --json title,number --template '- {{.title}} (#{{.number}})')"
 sed -i.tmp "s|Unreleased|Unreleased\n$title|" CHANGELOG.md
 rm CHANGELOG.md.tmp
+git add -p
+git commit -m "Update changelog"
