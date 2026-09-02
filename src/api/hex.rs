fdc38795d5e5909f3ee3425e0d0060d0e79acc7d
Vincent Ollivier <v@vinc.cc>
2026-09-02 23:25:49 +0200

    Use Display for HexDump

--- src/api/hex.rs
+++ src/api/hex.rs
@@ -3,41 +3,53 @@ use super::console::Style;
 use alloc::format;
 use alloc::string::String;
 use alloc::vec::Vec;
+use core::fmt;
 
-pub fn format_hex(buf: &[u8]) -> String {
+pub struct HexDump<'a> {
+    buf: &'a [u8],
+    offset: usize,
+}
+
+pub fn format_hex(buf: &[u8]) -> HexDump<'_> {
     format_hex_at(buf, 0)
 }
 
-pub fn format_hex_at(buf: &[u8], offset: usize) -> String {
-    let null = 0 as char;
-    let cyan = Style::color("aqua");
-    let gray = Style::color("gray");
-    let pink = Style::color("fushia");
-    let reset = Style::reset();
+pub fn format_hex_at(buf: &[u8], offset: usize) -> HexDump<'_> {
+    HexDump { buf, offset }
+}
 
-    let mut res = String::new();
-    for (index, chunk) in buf.chunks(16).enumerate() {
-        let addr = offset + index * 16;
+impl fmt::Display for HexDump<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let null = 0 as char;
+        let cyan = Style::color("aqua");
+        let gray = Style::color("gray");
+        let pink = Style::color("fushia");
+        let reset = Style::reset();
 
-        let hex = chunk.chunks(2).map(|pair|
-            pair.iter().map(|byte|
-                format!("{:02X}", byte)
-            ).collect::<Vec<String>>().join("")
-        ).collect::<Vec<String>>().join(" ");
+        for (index, chunk) in self.buf.chunks(16).enumerate() {
+            let addr = self.offset + index * 16;
 
-        let ascii: String = chunk.iter().map(|byte|
-            if *byte >= 32 && *byte <= 126 {
-                *byte as char
-            } else {
-                null
-            }
-        ).collect();
+            let hex = chunk.chunks(2).map(|pair|
+                pair.iter().map(|byte|
+                    format!("{:02X}", byte)
+                ).collect::<Vec<String>>().join("")
+            ).collect::<Vec<String>>().join(" ");
 
-        let text = ascii.replace(null, &format!("{}.{}", gray, reset));
+            let ascii: String = chunk.iter().map(|byte|
+                if *byte >= 32 && *byte <= 126 {
+                    *byte as char
+                } else {
+                    null
+                }
+            ).collect();
 
-        res.push_str(&format!(
-            "{}{:08X}: {}{:40}{}{}\n", cyan, addr, pink, hex, reset, text
-        ));
+            let text = ascii.replace(null, &format!("{}.{}", gray, reset));
+            write!(
+                f, "{}{:08X}: {}{:40}{}{}\n",
+                cyan, addr, pink, hex, reset, text
+            )?;
+        }
+
+        Ok(())
     }
-    res
 }
