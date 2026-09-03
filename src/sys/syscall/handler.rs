78944f00a27ead0cd6cad97dbd44823d35d81123
Vincent Ollivier <v@vinc.cc>
2026-09-03 21:18:32 +0200

    Add i686 syscall handler wrapper

--- src/sys/syscall/handler.rs
+++ src/sys/syscall/handler.rs
@@ -4,10 +4,39 @@ use crate::sys::x86::int::InterruptFrame;
 
 use core::arch::naked_asm;
 
+#[cfg(target_arch = "x86")]
 #[unsafe(naked)]
 pub extern "C" fn handler() -> ! {
     naked_asm!(
-        "cld",            // Clear direction flag
+        "cld",                     // Clear direction flag
+        "push eax",
+        "push ebx",
+        "push ecx",
+        "push edx",
+        "push edi",
+        "mov eax, esp",           // Register list
+        "lea edx, [esp + 5 * 4]", // Interrupt frame (5 registers * 4 bytes)
+        "sti",                    // Enable interrupts during syscall
+        "push eax",               // Arg #2
+        "push edx",               // Arg #1
+        "call {}",
+        "add esp, 8",             // Caller cleans up convention (cdecl)
+        "cli",
+        "pop edi",
+        "pop edx",
+        "pop ecx",
+        "pop ebx",
+        "pop eax",
+        "iretd",
+        sym inner
+    );
+}
+
+#[cfg(target_arch = "x86_64")]
+#[unsafe(naked)]
+pub extern "C" fn handler() -> ! {
+    naked_asm!(
+        "cld",                     // Clear direction flag
         "push rax",
         "push rcx",
         "push rdx",
@@ -17,10 +46,9 @@ pub extern "C" fn handler() -> ! {
         "push r9",
         "push r10",
         "push r11",
-        "mov rsi, rsp",   // Arg #2: register list
-        "mov rdi, rsp",   // Arg #1: interrupt frame
-        "add rdi, 9 * 8", // 9 registers * 8 bytes
-        "sti",            // Enable interrupts during syscall
+        "mov rsi, rsp",           // Arg #2: register list
+        "lea rdi, [rsp + 9 * 8]", // Arg #1: interrupt frame (9 registers * 8 bytes)
+        "sti",                    // Enable interrupts during syscall
         "call {}",
         "cli",
         "pop r11",
