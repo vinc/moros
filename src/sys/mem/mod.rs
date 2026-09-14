2402be1b6173c2ad52e0c0928e08fa2b70d9d87f
Vincent Ollivier <v@vinc.cc>
2026-09-14 19:01:29 +0200

    Move mem::init_paging to mem::paging::init on i686

--- src/sys/mem/mod.rs
+++ src/sys/mem/mod.rs
@@ -19,17 +19,8 @@ use crate::sys::boot::MemoryMap;
 use crate::sys::pic;
 use crate::sys::x86::addr::{PhysAddr, VirtAddr};
 
-#[cfg(target_arch = "x86")]
-use crate::sys::x86::page::{PageTable, PageTableEntry};
-
 use core::sync::atomic::{AtomicUsize, Ordering};
 use spin::Once;
-
-#[cfg(target_arch = "x86")]
-use spin::Mutex;
-
-#[cfg(target_arch = "x86")]
-static KERNEL_PAGE_TABLE: Mutex<PageTable> = Mutex::new(PageTable::new());
 
 #[cfg(target_arch = "x86_64")]
 use x86_64::structures::paging::{OffsetPageTable, Translate};
@@ -102,7 +93,7 @@ pub fn init(memory_map: &MemoryMap, offset: u64) {
 
         bitmap::init_frame_allocator(&memory_map);
         heap::init_alloc(heap_addr as *mut u8, heap_size as usize);
-        init_paging();
+        paging::init();
     }
 
     #[cfg(target_arch = "x86_64")] // TODO: Remove
@@ -120,42 +111,6 @@ pub fn init(memory_map: &MemoryMap, offset: u64) {
     }
 
     pic::unmask(pic::KBD_IRQ);
-}
-
-// TODO: Move to paging module
-#[cfg(target_arch = "x86")]
-fn init_paging() {
-    if !crate::sys::cpu::has_pse() {
-        log!("MEM PSE unavailable: paging disabled");
-        return;
-    }
-
-    let addr = {
-        let mut page_table = KERNEL_PAGE_TABLE.lock();
-
-        let level = 2;
-        let flags = PageTableEntry::PRESENT
-                  | PageTableEntry::WRITABLE
-                  | PageTableEntry::LARGE; // PSE must be enabled
-
-        for (index, entry) in page_table.entries.iter_mut().enumerate() {
-            *entry = PageTableEntry::new(level, index, flags);
-        }
-
-        phys_addr(page_table.entries.as_ptr())
-    };
-
-    use crate::sys::x86::reg::{Cr0, Cr3, Cr4};
-    unsafe {
-        // Enable page size extension
-        Cr4::write(Cr4::read() | Cr4::PSE);
-
-        // Load page directory
-        Cr3::write(addr, 0);
-
-        // Enable paging and write protection
-        Cr0::write(Cr0::read() | Cr0::PG | Cr0::WP);
-    }
 }
 
 pub fn phys_mem_offset() -> usize {
@@ -192,49 +147,4 @@ pub fn virt_to_phys(addr: VirtAddr) -> Option<PhysAddr> {
 #[cfg(target_arch = "x86_64")]
 pub fn virt_to_phys(addr: VirtAddr) -> Option<PhysAddr> {
     mapper().translate_addr(addr.into()).map(|x| x.into())
-}
-
-#[test_case]
-fn test_control_registers() {
-    use crate::sys::x86::reg::{Cr0, Cr3, Cr4};
-
-    #[cfg(target_arch = "x86")]
-    {
-        assert_eq!(Cr4::read() & Cr4::PSE, Cr4::PSE);
-        assert_eq!(Cr4::read() & Cr4::PAE, 0);
-    }
-
-    #[cfg(target_arch = "x86_64")]
-    {
-        assert_eq!(Cr4::read() & Cr4::PSE, 0);
-        assert_eq!(Cr4::read() & Cr4::PAE, Cr4::PAE);
-    }
-
-    #[cfg(target_arch = "x86")]
-    assert_eq!(
-        Cr3::read().addr(),
-        phys_addr(KERNEL_PAGE_TABLE.lock().entries.as_ptr())
-    );
-
-    assert_eq!(Cr3::read().flags(), 0);
-
-    assert_eq!(Cr0::read() & Cr0::PG, Cr0::PG);
-    assert_eq!(Cr0::read() & Cr0::WP, Cr0::WP);
-}
-
-#[cfg(target_arch = "x86")]
-#[test_case]
-fn test_paging() {
-    let page_table = KERNEL_PAGE_TABLE.lock();
-
-    let flags = PageTableEntry::PRESENT
-              | PageTableEntry::WRITABLE
-              | PageTableEntry::LARGE;
-
-    let dirty = PageTableEntry::ACCESSED
-              | PageTableEntry::DIRTY;
-
-    assert_eq!(page_table.entries[0].0,    0x0000_0000 | flags | dirty);
-    assert_eq!(page_table.entries[1].0,    0x0040_0000 | flags);
-    assert_eq!(page_table.entries[1023].0, 0xFFC0_0000 | flags);
 }
--- src/sys/mem/paging.rs
+++ src/sys/mem/paging.rs
@@ -1,5 +1,6 @@
 use crate::sys::x86::reg::Cr3;
 
+#[cfg(target_arch = "x86_64")]
 use x86_64::structures::paging::{PageTable, PhysFrame};
 
 #[cfg(target_arch = "x86_64")]
@@ -17,4 +18,93 @@ pub unsafe fn create_page_table(frame: PhysFrame) -> &'static mut PageTable {
     let virt_addr = super::phys_to_virt(phys_addr.into());
     let page_table_ptr: *mut PageTable = virt_addr.as_mut_ptr();
     &mut *page_table_ptr // unsafe
+}
+
+#[cfg(target_arch = "x86")]
+use crate::sys::x86::page::{PageTable, PageTableEntry};
+
+#[cfg(target_arch = "x86")]
+use spin::Mutex;
+
+#[cfg(target_arch = "x86")]
+static KERNEL_PAGE_TABLE: Mutex<PageTable> = Mutex::new(PageTable::new());
+
+#[cfg(target_arch = "x86")]
+pub fn init() {
+    if !crate::sys::cpu::has_pse() {
+        log!("MEM PSE unavailable: paging disabled");
+        return;
+    }
+
+    let addr = {
+        let mut page_table = KERNEL_PAGE_TABLE.lock();
+
+        let level = 2;
+        let flags = PageTableEntry::PRESENT
+                  | PageTableEntry::WRITABLE
+                  | PageTableEntry::LARGE; // PSE must be enabled
+
+        for (index, entry) in page_table.entries.iter_mut().enumerate() {
+            *entry = PageTableEntry::new(level, index, flags);
+        }
+
+        super::phys_addr(page_table.entries.as_ptr())
+    };
+
+    use crate::sys::x86::reg::{Cr0, Cr4};
+    unsafe {
+        // Enable page size extension
+        Cr4::write(Cr4::read() | Cr4::PSE);
+
+        // Load page directory
+        Cr3::write(addr, 0);
+
+        // Enable paging and write protection
+        Cr0::write(Cr0::read() | Cr0::PG | Cr0::WP);
+    }
+}
+
+#[test_case]
+fn test_control_registers() {
+    use crate::sys::x86::reg::{Cr0, Cr3, Cr4};
+
+    #[cfg(target_arch = "x86")]
+    {
+        assert_eq!(Cr4::read() & Cr4::PSE, Cr4::PSE);
+        assert_eq!(Cr4::read() & Cr4::PAE, 0);
+    }
+
+    #[cfg(target_arch = "x86_64")]
+    {
+        assert_eq!(Cr4::read() & Cr4::PSE, 0);
+        assert_eq!(Cr4::read() & Cr4::PAE, Cr4::PAE);
+    }
+
+    #[cfg(target_arch = "x86")]
+    assert_eq!(
+        Cr3::read().addr(),
+        super::phys_addr(KERNEL_PAGE_TABLE.lock().entries.as_ptr())
+    );
+
+    assert_eq!(Cr3::read().flags(), 0);
+
+    assert_eq!(Cr0::read() & Cr0::PG, Cr0::PG);
+    assert_eq!(Cr0::read() & Cr0::WP, Cr0::WP);
+}
+
+#[cfg(target_arch = "x86")]
+#[test_case]
+fn test_paging() {
+    let page_table = KERNEL_PAGE_TABLE.lock();
+
+    let flags = PageTableEntry::PRESENT
+              | PageTableEntry::WRITABLE
+              | PageTableEntry::LARGE;
+
+    let dirty = PageTableEntry::ACCESSED
+              | PageTableEntry::DIRTY;
+
+    assert_eq!(page_table.entries[0].0,    0x0000_0000 | flags | dirty);
+    assert_eq!(page_table.entries[1].0,    0x0040_0000 | flags);
+    assert_eq!(page_table.entries[1023].0, 0xFFC0_0000 | flags);
 }
