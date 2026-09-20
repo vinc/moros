427fc1a60afbf094f9bcaaa9d50e89036d89a51c
Vincent Ollivier <v@vinc.cc>
2026-09-20 19:55:13 +0200

    Add count limit

--- src/usr/mines.rs
+++ src/usr/mines.rs
@@ -267,6 +267,10 @@ pub fn main(args: &[&str]) -> Result<(), ExitCode> {
                 if i + 1 < n {
                     i += 1;
                     count = args[i].parse().unwrap_or(10);
+                    if count > 64 {
+                        error!("Invalid --count <num>");
+                        return Err(ExitCode::UsageError);
+                    }
                 } else {
                     error!("Missing --count <num>");
                     return Err(ExitCode::UsageError);
