9903a33361f88bbe931962cc8368823126fa8368
Vincent Ollivier <v@vinc.cc>
2026-09-23 22:21:10 +0200

    Add MemoryMap#reserve

--- src/sys/boot/mod.rs
+++ src/sys/boot/mod.rs
@@ -102,6 +102,37 @@ impl MemoryMap {
         self.len -= 1;
     }
 
+    pub fn reserve(&mut self, addr: u64, size: u64) {
+        let end = addr + size;
+        let mut i = 0;
+        while i < self.len {
+            let r = self.regions[i];
+            let r_end = r.addr + r.size;
+            if r_end <= addr || end <= r.addr {
+                // Keep region outside of reservation
+            } else if addr <= r.addr && r_end <= end {
+                // Remove region inside of reservation
+                self.remove(i);
+                continue;
+            } else if r.addr < addr && end < r_end {
+                // Split region around reservation
+                self.regions[i].size = addr - r.addr;
+                self.insert(i + 1, MemoryRegion::new(end, r_end - end, r.kind));
+            } else if r.addr < addr {
+                // Clip end of region
+                self.regions[i].size = addr - r.addr;
+            } else {
+                // Clip start of region
+                self.regions[i].addr = end;
+                self.regions[i].size = r_end - end;
+            }
+            i += 1;
+        }
+        let i = self.iter().position(|r| addr < r.addr).unwrap_or(self.len);
+        let region = MemoryRegion::new(addr, size, MemoryRegionType::Reserved);
+        self.insert(i, region);
+    }
+
     pub fn iter(&self) -> core::slice::Iter<'_, MemoryRegion> {
         self.regions[..self.len].iter()
     }
@@ -171,4 +202,60 @@ fn test_memory_map_remove() {
 
     assert_eq!(memory_map.regions[0].addr, 1024);
     assert_eq!(memory_map.regions[0].size, 1024);
+}
+
+#[test_case]
+fn test_memory_map_reserve_span() {
+    let mut memory_map = MemoryMap::new();
+
+    memory_map.add(MemoryRegion::new(0, 1024, MemoryRegionType::Kernel));
+    memory_map.add(MemoryRegion::new(1024, 1024, MemoryRegionType::Usable));
+    memory_map.add(MemoryRegion::new(2048, 4096, MemoryRegionType::Usable));
+
+    assert_eq!(memory_map.len, 3);
+    memory_map.reserve(512, 2048);
+    assert_eq!(memory_map.len, 3);
+
+    assert_eq!(memory_map.regions[0].addr, 0);
+    assert_eq!(memory_map.regions[1].addr, 512);
+    assert_eq!(memory_map.regions[2].addr, 2048 + 512);
+
+    assert_eq!(memory_map.regions[0].size, 512);
+    assert_eq!(memory_map.regions[1].size, 2048);
+    assert_eq!(memory_map.regions[2].size, 4096 - 512);
+
+    assert_eq!(memory_map.regions[0].kind, MemoryRegionType::Kernel);
+    assert_eq!(memory_map.regions[1].kind, MemoryRegionType::Reserved);
+    assert_eq!(memory_map.regions[2].kind, MemoryRegionType::Usable);
+}
+
+#[test_case]
+fn test_memory_map_reserve_split() {
+    let mut memory_map = MemoryMap::new();
+
+    memory_map.add(MemoryRegion::new(0, 1024, MemoryRegionType::Kernel));
+    memory_map.add(MemoryRegion::new(1024, 4096, MemoryRegionType::Usable));
+    memory_map.add(MemoryRegion::new(8192, 1024, MemoryRegionType::Reserved));
+
+    assert_eq!(memory_map.len, 3);
+    memory_map.reserve(2048, 1024);
+    assert_eq!(memory_map.len, 5);
+
+    assert_eq!(memory_map.regions[0].addr, 0);
+    assert_eq!(memory_map.regions[1].addr, 1024);
+    assert_eq!(memory_map.regions[2].addr, 2048);
+    assert_eq!(memory_map.regions[3].addr, 3072);
+    assert_eq!(memory_map.regions[4].addr, 8192);
+
+    assert_eq!(memory_map.regions[0].size, 1024);
+    assert_eq!(memory_map.regions[1].size, 1024);
+    assert_eq!(memory_map.regions[2].size, 1024);
+    assert_eq!(memory_map.regions[3].size, 2048);
+    assert_eq!(memory_map.regions[4].size, 1024);
+
+    assert_eq!(memory_map.regions[0].kind, MemoryRegionType::Kernel);
+    assert_eq!(memory_map.regions[1].kind, MemoryRegionType::Usable);
+    assert_eq!(memory_map.regions[2].kind, MemoryRegionType::Reserved);
+    assert_eq!(memory_map.regions[3].kind, MemoryRegionType::Usable);
+    assert_eq!(memory_map.regions[4].kind, MemoryRegionType::Reserved);
 }
